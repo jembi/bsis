5a3da873c2793e798c2f429ab1904d1b3c195569
Daniel Futerman <daniel.futerman@gmail.com>
2014-03-31 15:13:21 +0200

    Ignore unused Controller tests

--- test/controller/UserControllerTest.java
+++ test/controller/UserControllerTest.java
@@ -4,6 +4,7 @@ package controller;
 import model.user.User;
 
 import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -21,7 +22,7 @@ public class UserControllerTest {
 	public UserRepository userRepository;
 	
 
-	@Test
+	@Ignore @Test
 	public void testAddUser() {
 		try{
 			User user = new User();
@@ -36,7 +37,7 @@ public class UserControllerTest {
 		}
 	}
 	
-	@Test
+	@Ignore @Test
 	public void testUpdateUser() {
 		try{
 			User user = userRepository.findUserById(2);
