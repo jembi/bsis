101ed290e8a4a13d3f2293ecb930ce5f8b7014d4
srikanth <srireddymalyala@gmail.com>
2014-04-05 01:08:36 +0200

    Added  Test Scripts on Donations(#158)
    --- Find Donation--
    --Add Donation--
    --Find Donation Batch--
    --New Donation Batch--
    
    ---------------------------------
    
    Still need to be added worksheet test scripts and to use constants in
    place hard coded selectors

--- test/scripts/DonationsTest.js
+++ test/scripts/DonationsTest.js
@@ -4,7 +4,7 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////  TEST  1 --> Login Test into BSIS  //////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
-casper.options.waitTimeout = {width: '1600', height: '1600'};
+
 casper.test.begin('Login Test to BSIS',1,function(test){
 casper.start(LOGIN_URL, function() {
     this.fillSelectors('form#loginAction', {
@@ -27,38 +27,33 @@ casper.run(function() {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////  TEST 3 - Add Donation Record  //////////////////////////////
+///////////////////////////  TEST 3 - Find Donation Record  //////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
 
+casper.test.begin('Find Donations Record',8,function(test){
+  casper.start('http://localhost:8080/bsis/welcomePage.html').viewport(1000,1000);
 
+ casper.then(function(){
+    var tabSelector  =     this.evaluate(function(){
 
+          for(var i=1;i<10;i++){
+         var  selector =  "#topPanelTabs li:nth-child("+i+") a";
+          if(document.querySelector(selector).textContent ==='Donations'){
+           return selector;
+       }
+    }
+    return null;
+ });
 
-casper.test.begin('Add  Donation Record',9,function(test){
-  casper.start('http://localhost:8080/bsis/welcomePage.html').viewport(1600,1000);
+        casper.click(tabSelector);
+       casper.waitForSelector('.formFormatClass', function success(){
+        test.pass('Donors Page Loaded Successfully') },function timeout(){
+          test.fail('Donors page timeout')},TIMEOUT);
 
-casper.then(function(){
-  var tabSelector  =     this.evaluate(function(){
-
-         for(var i=1;i<10;i++){
-        var  selector =  "#topPanelTabs li:nth-child("+i+")";
-         if(document.querySelector(selector).textContent ==='Donations'){
-          return selector;
-      }
-   }
-   return null;
-});
-  this.echo(tabSelector);
-  casper.click(tabSelector);
-});
-
-
-casper.then(function(){
-      casper.waitForSelector('.formDiv', function success(){
-        test.pass('Donations Page Loaded Successfully') },function timeout(){
-          test.fail('Donations page timeout')},TIMEOUT);
+       casper.wait('5000');
    
-    });
 
+});
 
 casper.then(function(){
 
@@ -81,15 +76,128 @@ casper.then(function(){
 
 
 
+
 casper.run(function() {
-    this.echo('Test Successful - Add DOnation Record', 'INFO');
+    this.echo('Test Successful - Find DOnation Record', 'INFO');
        test.done();
    });
 
 
 });
 
+////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////  TEST 3 - Add  Donation Record  //////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
+casper.test.begin('Add Donations Record',14,function(test){
+
+ casper.then(function(){
+
+casper.click('#addCollectionsContent a');
+  casper.waitForSelector('#donorWeight', function success(){
+        test.pass('Donors Page Loaded Successfully') },function timeout(){
+          test.fail('Donors page timeout')},TIMEOUT);
+
+       
+ });
+
+casper.then(function(){
+//Asserting Input fields
+test.assertExists('#collectionNumber');
+test.assertExists('#collectionBatchNumber');
+test.assertExists('#donorNumber');
+test.assertExists('#collectedOn');
+test.assertExists('.addCollectionFormDonationType');
+ // test.assertExists('#bloodBagType');
+test.assertExists('.addCollectionFormCenters');
+test.assertExists('#donorWeight');
+test.assertExists('#donorPulse');
+test.assertExists('#bloodPressureSystolic');
+test.assertExists('#bloodPressureDiastolic');
+test.assertExists('#notes');
+
+// Asserting buttons
+test.assertExists('.addCollectionButton');
+test.assertExists('.clearFormButton');
+
+});
+
+casper.run(function() {
+    this.echo('Test Successful - Add DOnation Record', 'INFO');
+       test.done();
+   });
+});
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////  TEST 4- Add  Donation Batch  //////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+casper.test.begin('Add Donations Record',6,function(test){
+
+ casper.then(function(){
+
+casper.click('#findCollectionBatchContent a');
+  casper.waitForText('Find Collection Batches', function success(){
+        test.pass('Find Collection Batches Page Loaded Successfully') },function timeout(){
+          test.fail('Find Collection Batches page timeout')},TIMEOUT);
+
+       
+ });
+
+casper.then(function(){
+
+//Asserting fields
+test.assertExists('#batchNumber');
+test.assertExists('.collectionCenterSelector');
+test.assertExists('.collectionSiteSelector');
+
+//asserting buttons
+test.assertExists('.findCollectionBatchButton');
+test.assertExists('.clearFindFormButton ');
+
+});
+
+casper.run(function() {
+    this.echo('Test Successful -  Find Donation Batche', 'INFO');
+       test.done();
+   });
+});
+
+////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////  TEST 5- Add  Donation Batch  //////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+casper.test.begin('New Donations Record',6,function(test){
+
+ casper.then(function(){
+
+casper.click('#createCollectionBatch a');
+  casper.waitForSelector('.addCollectionBatchFormCenters', function success(){
+        test.pass('New Collection Batches Page Loaded Successfully') },function timeout(){
+          test.fail('New Collection Batches page timeout')},TIMEOUT);
+
+       
+ });
+
+casper.then(function(){
+
+//Asserting fields
+test.assertExists('.addCollectionBatchFormCenters');
+test.assertExists('.addCollectionBatchFormSites');
+test.assertExists('#notes');
+
+//asserting buttons
+test.assertExists('.addCollectionBatchButton');
+test.assertExists('.clearFormButton');
+
+});
+
+casper.run(function() {
+    this.echo('Test Successful -  New Donation Batche', 'INFO');
+       test.done();
+   });
+});
 
 
 
