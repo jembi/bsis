491cdeb843b494fe9b8e50341ed2d667ea7d0129
srikanth <srireddymalyala@gmail.com>
2014-06-12 11:52:21 +0200

    Fixed null pointer exception in updating centers/sites form(#19)

--- src/viewmodel/LocationViewModel.java
+++ src/viewmodel/LocationViewModel.java
@@ -19,22 +19,22 @@ public class LocationViewModel {
   }
 
   public String getIsCollectionCenter() {
-    return location.getIsCollectionCenter().toString();
+    return location.getIsCollectionCenter()!=null? location.getIsCollectionCenter().toString(): "";
   }
 
   public String getIsCollectionSite() {
-    return location.getIsCollectionSite().toString();
+    return location.getIsCollectionSite()!=null?location.getIsCollectionSite().toString():"";
   }
 
   public String getIsUsageSite() {
-    return location.getIsUsageSite().toString();
+    return location.getIsUsageSite()? location.getIsUsageSite().toString(): "";
   }
 
   public String getIsMobileSite() {
-    return location.getIsMobileSite().toString();
+    return location.getIsMobileSite()!=null? location.getIsMobileSite().toString(): "";
   }
 
   public String getIsDonorPanel() {
-    return location.getIsDonorPanel().toString();
+    return location.getIsDonorPanel()!=null?location.getIsDonorPanel().toString():"";
   }
 }
