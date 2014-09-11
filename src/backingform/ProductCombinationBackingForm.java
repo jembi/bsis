e94aa58661e971d62f22d34d7f0c7254078d4b35
srikanth <srikanth@jembi.org>
2014-09-11 16:44:13 +0200

    Removed unnecessary response of addProductcombination end point (#209)

--- src/backingform/ProductCombinationBackingForm.java
+++ src/backingform/ProductCombinationBackingForm.java
@@ -1,5 +1,6 @@
 package backingform;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
@@ -28,6 +29,7 @@ public class ProductCombinationBackingForm {
 
   @NotNull
   @Valid
+  @JsonIgnore
   private Product product;
 
   private String createdOn;
@@ -56,10 +58,12 @@ public class ProductCombinationBackingForm {
     return product.getId();
   }
 
+  @JsonIgnore
   public CollectedSample getCollectedSample() {
     return product.getCollectedSample();
   }
 
+  
   public Date getLastUpdated() {
     return product.getLastUpdated();
   }
@@ -68,10 +72,12 @@ public class ProductCombinationBackingForm {
     return product.getCreatedDate();
   }
 
+  @JsonIgnore
   public User getCreatedBy() {
     return product.getCreatedBy();
   }
 
+  @JsonIgnore
   public User getLastUpdatedBy() {
     return product.getLastUpdatedBy();
   }
