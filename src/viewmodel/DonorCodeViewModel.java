625fe30b49c5e841f07f77807f9a272089333e5e
srikanth <srikanth@jembi.org>
2014-10-10 16:04:18 +0200

    Fixed get id of donor code view model (#209)

--- src/viewmodel/DonorCodeViewModel.java
+++ src/viewmodel/DonorCodeViewModel.java
@@ -23,7 +23,7 @@ public class DonorCodeViewModel {
         this.donorDonorCode = donorDonorCode;
     }
     
-    public Long donorCodeId(){
+    public Long getDonorCodeId(){
         return donorDonorCode.getDonorCode().getId();
     }
     
