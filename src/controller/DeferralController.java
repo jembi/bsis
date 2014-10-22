6eacecdc4982ef97fd48f22cbbc11a4d2271e09a
srikanth <srikanth@jembi.org>
2014-10-22 12:40:25 +0200

    Normalized imports on deferrals (#209)

--- src/controller/DeferralController.java
+++ src/controller/DeferralController.java
@@ -6,7 +6,6 @@
 package controller;
 
 import backingform.DeferralBackingForm;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,12 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import repository.DonorRepository;
 import utils.PermissionConstants;
