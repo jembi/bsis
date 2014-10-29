3500269e0e76dd45601983d3a3dd304da93c752d
srikanth <srikanth@jembi.org>
2014-10-29 12:31:09 +0200

    Added test batch backing form class (#229)

---
+++ src/backingform/TestBatchBackingForm.java
@@ -0,0 +1,18 @@
+
+package backingform;
+
+public class TestBatchBackingForm {
+  
+    
+    private String firstDIN;
+    private String lastDIN;
+
+    public void setFirstDIN(String firstDIN) {
+        this.firstDIN = firstDIN;
+    }
+
+    public void setLastDIN(String lastDIN) {
+        this.lastDIN = lastDIN;
+    }
+    
+}
