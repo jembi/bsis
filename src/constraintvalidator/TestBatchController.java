99482e6896e5878a9810f65cd516bfb21093942f
srikanth <srikanth@jembi.org>
2014-10-29 13:06:51 +0200

    Updated add & search end points (#229)

--- src/constraintvalidator/TestBatchController.java
+++ src/constraintvalidator/TestBatchController.java
@@ -1,13 +1,18 @@
 package constraintvalidator;
 
+import backingform.TestBatchBackingForm;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import repository.SequenceNumberRepository;
 import repository.TestBatchRepository;
 
 @RestController
@@ -16,20 +21,41 @@ public class TestBatchController {
 
     @Autowired
     private TestBatchRepository testBatchRepository;
+    
+    @Autowired
+    private SequenceNumberRepository sequenceNumberRepository;
 
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity addTestBatch() {
+    public ResponseEntity addTestBatch(@RequestBody TestBatchBackingForm form) {
         
+        testBatchRepository.saveTestBatch(form.getFirstDIN(), form.getLastDIN(), getNextTestBatchNumber());
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity findTestBatchPagination() {
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public ResponseEntity findTestBatchPagination(
+            @RequestParam("firstDIN") String firstDIN,
+            @RequestParam("lastDIN") String lastDIN,
+            @RequestParam("testBatchNumber") String testBatchNumber,
+            @RequestParam("createdBeforeDate") String createdBeforeDate,
+            @RequestParam("createdAfterDate") String createdAfterDate) {
 
-        Map<String, Object> map = new HashMap<String, Object>();
-        return new ResponseEntity(map, HttpStatus.OK);
+        Map<String, Object> pagingParams = new HashMap<String, Object>();
+        pagingParams.put("sortColumn", "id");
+        pagingParams.put("sortDirection", "asc");
+        
+         List<Object> results = testBatchRepository.findCollectedSamplesByTestBatch(
+	    		testBatchNumber,
+	    		createdAfterDate, createdBeforeDate, pagingParams);
+        return new ResponseEntity(results, HttpStatus.OK);
 
     }
+    
+    public String getNextTestBatchNumber() {
+        return sequenceNumberRepository.getNextTestBatchNumber();
+    }
+    
+    
 
 }
