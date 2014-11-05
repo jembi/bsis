65856d096f745745a391d52229fd1aa4fdf4e066
Daniel Futerman <daniel.futerman@gmail.com>
2014-11-06 00:31:03 +0200

    Add findTestResultsForTestBatch endpoint to TestResultController

--- src/controller/TestResultController.java
+++ src/controller/TestResultController.java
@@ -2,8 +2,12 @@ package controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import model.collectedsample.CollectedSample;
+import model.collectionbatch.CollectionBatch;
+import model.testbatch.TestBatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import repository.CollectedSampleRepository;
+import repository.TestBatchRepository;
 import repository.bloodtesting.BloodTestingRepository;
 import utils.PermissionConstants;
 import viewmodel.CollectedSampleViewModel;
@@ -25,6 +31,9 @@ public class TestResultController {
   @Autowired
   private CollectedSampleRepository collectedSampleRepository;
 
+  @Autowired
+  private TestBatchRepository testBatchRepository;
+  
   @Autowired
   private BloodTestingRepository bloodTestingRepository;
   
@@ -65,5 +74,27 @@ public class TestResultController {
     map.put("donation", new CollectedSampleViewModel(c));
     map.put("overview", results);
     return new ResponseEntity(map, HttpStatus.OK);
+  }
+  
+  @RequestMapping(value = "/results/list", method = RequestMethod.GET)
+  @PreAuthorize("hasRole('"+PermissionConstants.VIEW_TTI_OUTCOME+"')")
+  public ResponseEntity findTestResultsForTestBatch(HttpServletRequest request,
+		@RequestParam(value = "testBatch", required = true) Long testBatchId) {
+	  
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		TestBatch testBatch = testBatchRepository.findTestBatchById(testBatchId);
+		List<CollectionBatch> collectionBatches = testBatch.getCollectionBatches();
+		List<Integer> donationBatchIds = new ArrayList<Integer>();
+		for(CollectionBatch collectionBatch : collectionBatches){
+			donationBatchIds.add(collectionBatch.getId());
+		}
+	
+	    List<BloodTestingRuleResult> ruleResults =
+	    		bloodTestingRepository.getAllTestsStatusForDonationBatches(donationBatchIds);
+	
+		map.put("testresults", ruleResults);
+	
+		return new ResponseEntity(map, HttpStatus.OK);
   }
 }
