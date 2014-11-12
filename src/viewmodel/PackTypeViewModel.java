2ce57818bff7a86a964c8ed3b0ad44a392bbfb7d
srikanth <srikanth@jembi.org>
2014-11-12 12:01:13 +0200

    Add pack type view model (#234)

---
+++ src/viewmodel/PackTypeViewModel.java
@@ -0,0 +1,43 @@
+
+
+package viewmodel;
+
+import model.bloodbagtype.BloodBagType;
+
+public class PackTypeViewModel {
+    
+    private BloodBagType packType;
+
+    public PackTypeViewModel(BloodBagType packType) {
+        this.packType = packType;
+    }
+    
+    public Integer getId(){
+        return packType.getId();
+    }
+    
+    public String getBloodBagType(){
+        return packType.getBloodBagType();
+    }
+    
+    public ProductTypeViewModel getProductType(){
+        if (packType.getProductType() != null) {
+            return new ProductTypeViewModel(packType.getProductType());
+        } else {
+            return null;
+        }
+    }
+    
+    public Boolean getCanPool(){
+        return packType.getCanPool();
+    }
+    
+    public Boolean getCanSplit(){
+        return packType.getCanSplit();
+    }
+    
+    public Boolean get(){
+        return packType.getIsDeleted();
+    }
+    
+}
