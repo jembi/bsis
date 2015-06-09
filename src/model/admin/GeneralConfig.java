4b94a879850d431f333795eba9811e9226579508
Duma Mtungwa <dumakude@gmail.com>
2015-06-09 14:44:14 +0200

    added unique constraint

--- src/model/admin/GeneralConfig.java
+++ src/model/admin/GeneralConfig.java
@@ -7,12 +7,7 @@
 package model.admin;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 /**
  *
@@ -25,7 +20,9 @@ public class GeneralConfig implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @Column(unique=true)
     private String name;
+
     private String value;
     private String description ;
 
