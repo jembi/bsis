4072edcd5d4b930080ab294cf8bb21b8c1e5dd0e
Duma Mtungwa <dumakude@gmail.com>
2015-07-29 11:09:50 +0200

    added sheduled task to update the general config at startup

---
+++ src/tasks/GeneralConfigUpdater.java
@@ -0,0 +1,46 @@
+package tasks;
+
+import com.google.gson.Gson;
+import model.admin.GeneralConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import repository.GeneralConfigRepository;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+
+@Component
+public class GeneralConfigUpdater {
+
+    @Autowired
+    private ServletContext servletContext;
+
+    @Autowired
+    private GeneralConfigRepository generalConfigRepository;
+
+    public GeneralConfigUpdater() {
+
+    }
+
+    @Scheduled(fixedDelay = Integer.MAX_VALUE)
+    public void applyCustomConfigsFromJSON () throws IOException {
+        try {
+            
+            Reader reader = new InputStreamReader(servletContext.getResourceAsStream("/WEB-INF/general-configs.json"), "UTF-8");
+            Gson gson = new Gson();
+            GeneralConfig[] generalConfigsArray = gson.fromJson(reader, GeneralConfig[].class);
+
+            for (GeneralConfig temp : generalConfigsArray) {
+                System.out.println("Updating general config from file: " + temp.getDescription());
+                generalConfigRepository.update(temp);
+            }
+
+        } catch (IOException error){
+            System.out.println(error);
+        }
+    }
+}
