e1ec3ce68b731bb4eee965118deff5021a95af06
Duma Mtungwa <dumakude@gmail.com>
2015-08-19 15:27:06 +0200

    added BloodTestBackingFormValidator

---
+++ src/backingform/validator/BloodTestBackingFormValidator.java
@@ -0,0 +1,42 @@
+package backingform.validator;
+
+import backingform.BloodTestBackingForm;
+import controller.UtilController;
+import model.bloodtesting.BloodTest;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import java.util.Arrays;
+
+public class BloodTestBackingFormValidator implements Validator {
+
+    private Validator validator;
+    private UtilController utilController;
+
+
+    public BloodTestBackingFormValidator(Validator validator, UtilController utilController){
+        super();
+        this.validator = validator;
+        this.utilController = utilController;
+    }
+
+    public boolean supports(Class<?> clazz) {
+        return Arrays.asList(BloodTestBackingForm.class, BloodTest.class).contains(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.invokeValidator(validator, target, errors);
+        BloodTestBackingForm form = (BloodTestBackingForm) target;
+
+        ValidationUtils.rejectIfEmpty(errors, "BloodTest.testName", "testName.empty", "The testName is required");
+        ValidationUtils.rejectIfEmpty(errors, "BloodTest.bloodTestCategory", "bloodTestCategory.empty", "The bloodTestCategory is required");
+        ValidationUtils.rejectIfEmpty(errors, "BloodTest.validResults", "validResults.empty", "The validResults is required");
+        ValidationUtils.rejectIfEmpty(errors, "BloodTest.positiveResults", "positiveResults.empty", "The positiveResults is required");
+        ValidationUtils.rejectIfEmpty(errors, "BloodTest.negativeResults", "negativeResults.empty", "The negativeResults is required");
+        ValidationUtils.rejectIfEmpty(errors, "BloodTest.testNameShort", "testNameShort.empty", "The testNameShort is required");
+        ValidationUtils.rejectIfEmpty(errors, "BloodTest.numberOfConfirmatoryTests", "numberOfConfirmatoryTests.empty", "The numberOfConfirmatoryTests is required");
+        ValidationUtils.rejectIfEmpty(errors, "BloodTest.rankInCategory", "rankInCategory.empty", "The rankInCategory is required");
+    }
+}
