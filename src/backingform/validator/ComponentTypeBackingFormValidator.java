cd28cb2c7569c277ef7c88c97919d186d3058beb
Brett Ausmeier <brett.ausmeier@jembi.org>
2015-08-21 10:30:04 +0200

    Fix formatting

--- src/backingform/validator/ComponentTypeBackingFormValidator.java
+++ src/backingform/validator/ComponentTypeBackingFormValidator.java
@@ -1,20 +1,17 @@
 package backingform.validator;
 
-
 import backingform.ComponentTypeBackingForm;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-
-
 import java.util.Arrays;
 
-public class ComponentTypeBackingFormValidator  implements Validator {
+public class ComponentTypeBackingFormValidator implements Validator {
 
     private Validator validator;
 
-    public ComponentTypeBackingFormValidator(Validator validator){
+    public ComponentTypeBackingFormValidator(Validator validator) {
         super();
         this.validator = validator;
     }
@@ -25,21 +22,15 @@ public class ComponentTypeBackingFormValidator  implements Validator {
     }
 
     @Override
-    public void validate(Object obj, Errors errors){
+    public void validate(Object obj, Errors errors) {
 
         ValidationUtils.invokeValidator(validator, obj, errors);
-
 
         ValidationUtils.rejectIfEmpty(errors, "componentType.componentTypeName", "componentTypeName.empty", "The componentTypeName is required");
         ValidationUtils.rejectIfEmpty(errors, "componentType.componentTypeNameShort", "componentTypeNameShort.empty", "The componentTypeNameShort is required");
         ValidationUtils.rejectIfEmpty(errors, "componentType.expiresAfter", "expiresAfter.empty", "The expiresAfter value is required");
         ValidationUtils.rejectIfEmpty(errors, "componentType.expiresAfterUnits", "expiresAfterUnits.empty", "The expiresAfterUnits value is required");
         ValidationUtils.rejectIfEmpty(errors, "componentType.description", "description.empty", "The description is required");
-
     }
-
-
-
-
 
 }
