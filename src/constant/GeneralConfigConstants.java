7381fb38ef05fa5c3508e6a603d24b39c3658d67
Brett Ausmeier <brett.ausmeier@jembi.org>
2015-10-23 10:36:51 +0200

    Add general config for creating initial components
    Adds a general config property that can be used to disable the creation
    of initial components in the case that the components are not managed by
    the system.

--- src/constant/GeneralConfigConstants.java
+++ src/constant/GeneralConfigConstants.java
@@ -4,5 +4,6 @@ public class GeneralConfigConstants {
     
     public static final String DONOR_REGISTRATION_OPEN_BATCH_REQUIRED = "donors.registration.openBatchRequired";
     public static final String DEFER_DONORS_WITH_NEG_CONFIRMATORY_OUTCOMES = "testing.deferDonorsWithNegConfirmatoryOutcomes";
+    public static final String CREATE_INITIAL_COMPONENTS = "components.createInitialComponents";
 
 }
