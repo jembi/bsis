13d66c0721e7f36b507386ed2500c9f4f9fc535e
Brett Ausmeier <brett.ausmeier@jembi.org>
2015-11-05 13:45:49 +0200

    Include isEligible in donor overview

--- src/controller/DonorController.java
+++ src/controller/DonorController.java
@@ -158,6 +158,7 @@ public class DonorController {
     map.put("flaggedForCounselling", flaggedForCounselling);
     map.put("deferredUntil",CustomDateFormatter.getDateString(donorRepository.getLastDonorDeferralDate(id)));
 	map.put("canDelete", donorConstraintChecker.canDeleteDonor(id));
+	map.put("isEligible", donorConstraintChecker.isDonorEligibleToDonate(id));
     if(donations.size() > 0){
 	    map.put("lastDonation", getDonationViewModel(donations.get(donations.size()-1)));
 	    map.put("dateOfFirstDonation",CustomDateFormatter.getDateString(donations.get(0).getDonationDate()));
