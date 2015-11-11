caafe290a53d9cc2ced3819973762975abf0bacd
Dagmar Timler <dagmar.timler@gmail.com>
2015-11-11 09:08:38 +0200

    added unit tests for DonationBatchViewModelFactory and DonationBatchConstraintChecker

--- test/helpers/builders/DonationBatchBuilder.java
+++ test/helpers/builders/DonationBatchBuilder.java
@@ -5,6 +5,7 @@ import java.util.List;
 import model.donation.Donation;
 import model.donationbatch.DonationBatch;
 import model.location.Location;
+import model.testbatch.TestBatch;
 
 public class DonationBatchBuilder extends AbstractEntityBuilder<DonationBatch> {
 
@@ -12,6 +13,7 @@ public class DonationBatchBuilder extends AbstractEntityBuilder<DonationBatch> {
 	private String batchNumber;
     private List<Donation> donations;
     private Location venue;
+    private TestBatch testBatch;
     private boolean deleted;
     private boolean closed;
     private boolean backEntry;
@@ -40,6 +42,11 @@ public class DonationBatchBuilder extends AbstractEntityBuilder<DonationBatch> {
     	this.venue = venue;
     	return this;
     }
+    
+    public DonationBatchBuilder withTestBatch(TestBatch testBatch) {
+    	this.testBatch = testBatch;
+    	return this;
+    }
 
     public DonationBatchBuilder withBatchNumber(String batchNumber) {
     	this.batchNumber = batchNumber;
@@ -61,6 +68,7 @@ public class DonationBatchBuilder extends AbstractEntityBuilder<DonationBatch> {
         donationBatch.setIsClosed(closed);
         donationBatch.setVenue(venue);
         donationBatch.setBackEntry(backEntry);
+        donationBatch.setTestBatch(testBatch);
         return donationBatch;
     }
     
