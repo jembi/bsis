3da096442fa9a373a99eef320244998d8db585f7
Brett Ausmeier <brett.ausmeier@jembi.org>
2015-11-27 14:49:06 +0200

    Remove age validation

--- src/controller/UtilController.java
+++ src/controller/UtilController.java
@@ -36,7 +36,6 @@ import org.springframework.beans.NotReadablePropertyException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.Errors;
-import constant.GeneralConfigConstants;
 import repository.DonationRepository;
 import repository.DonationBatchRepository;
 import repository.DonorRepository;
@@ -55,9 +54,7 @@ import repository.PackTypeRepository;
 import repository.DeferralReasonRepository;
 import repository.DiscardReasonRepository;
 import security.BsisUserDetails;
-import service.GeneralConfigAccessorService;
 import repository.DonationTypeRepository;
-import utils.DonorUtils;
 
 @org.springframework.stereotype.Component
 public class UtilController {
@@ -119,9 +116,6 @@ public class UtilController {
 
   @Autowired
   private DonationTypeRepository donationTypeRepository;
-  
-  @Autowired
-  private GeneralConfigAccessorService generalConfigAccessorService;
 
   public Map<String, Map<String, Object>> getFormFieldsForForm(String formName) {
     List<FormField> formFields = formFieldRepository.getFormFields(formName);
@@ -357,23 +351,6 @@ public class UtilController {
 	  else{
 		  return false;
 	  }
-  }
-
-  public String verifyDonorAge(Date birthDate) {
-    Integer donorAge = DonorUtils.computeDonorAge(birthDate);
-
-    if (donorAge == null) {
-      return "One of donor Date of Birth or Age must be specified";
-    }
-
-    int minAge = generalConfigAccessorService.getIntValue(GeneralConfigConstants.DONOR_MINIMUM_AGE);
-    int maxAge = generalConfigAccessorService.getIntValue(GeneralConfigConstants.DONOR_MAXIMUM_AGE);
-
-    if (donorAge < minAge || donorAge > maxAge) {
-      return "Donor age must be between " + minAge + " and " + maxAge + " years.";
-    }
-
-    return null;
   }
 
   public Component findComponent(String donationIdentificationNumber, String componentType) {
