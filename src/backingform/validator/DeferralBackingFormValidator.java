06fd81703315204cfba88baa8dc39420adc23a01
Duma Mtungwa <dumakude@gmail.com>
2016-02-23 10:59:04 +0200

    BSIS-424 Added validation to determin if venue exists

--- src/backingform/validator/DeferralBackingFormValidator.java
+++ src/backingform/validator/DeferralBackingFormValidator.java
@@ -1,17 +1,25 @@
 package backingform.validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
 import backingform.DeferralBackingForm;
+import repository.LocationRepository;
 
 @Component
 public class DeferralBackingFormValidator extends BaseValidator<DeferralBackingForm> {
+  @Autowired
+  LocationRepository locationRepository;
 
   @Override
   public void validateForm(DeferralBackingForm form, Errors errors) {
     if (form.getVenue() == null) {
       errors.rejectValue("venue", "deferral.venue.required", "Venue is required");
+    } else {
+      if (locationRepository.findById(form.getVenue().getId()) == null) {
+        errors.rejectValue("venue", "deferral.venue.required", "Venue does not exist");
+      }
     }
   }
 
