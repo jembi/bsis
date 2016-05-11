3475af40c92810783ac13e05ee1ce342448dfada
lauravignoli <vignoli.laura@gmail.com>
2016-05-11 12:36:10 +0200

    BSIS-884 Create generateStockLevelsReport method in ReportGeneratorService
    Note that we decided to use the ReportGeneratorService to generate reports from now on.

--- src/constant/CohortConstants.java
+++ src/constant/CohortConstants.java
@@ -5,5 +5,6 @@ public class CohortConstants {
   public static final String BLOOD_TYPE_CATEGORY = "Blood Type";
   public static final String DONATION_TYPE_CATEGORY = "Donation Type";
   public static final String GENDER_CATEGORY = "Gender";
+  public static final String COMPONENT_TYPE_CATEGORY = "Component Type";
 
 }
--- src/service/ReportGeneratorService.java
+++ src/service/ReportGeneratorService.java
@@ -9,18 +9,25 @@ import org.springframework.stereotype.Service;
 
 import constant.CohortConstants;
 import dto.CollectedDonationDTO;
+import dto.StockLevelDTO;
 import model.donationtype.DonationType;
+import model.inventory.InventoryStatus;
+import model.location.Location;
 import model.reporting.Cohort;
 import model.reporting.Comparator;
 import model.reporting.DataValue;
 import model.reporting.Report;
 import repository.DonationRepository;
+import repository.InventoryRepository;
 
 @Service
 public class ReportGeneratorService {
 
   @Autowired
   private DonationRepository donationRepository;
+  
+  @Autowired
+  private InventoryRepository inventoryRepository;
 
   /**
    * Report listing all donations collected within a selected date range by collection site,
@@ -58,6 +65,39 @@ public class ReportGeneratorService {
       genderCohort.setComparator(Comparator.EQUALS);
       genderCohort.setOption(dto.getGender());
       dataValue.addCohort(genderCohort);
+
+      Cohort bloodTypeCohort = new Cohort();
+      bloodTypeCohort.setCategory(CohortConstants.BLOOD_TYPE_CATEGORY);
+      bloodTypeCohort.setComparator(Comparator.EQUALS);
+      bloodTypeCohort.setOption(dto.getBloodAbo() + dto.getBloodRh());
+      dataValue.addCohort(bloodTypeCohort);
+
+      dataValues.add(dataValue);
+    }
+
+    report.setDataValues(dataValues);
+
+    return report;
+  }
+  
+  public Report generateStockLevelsReport(Location location, InventoryStatus inventoryStatus) {
+    Report report = new Report();
+
+    List<StockLevelDTO> dtos = inventoryRepository.findStockLevelsForLocation(location, inventoryStatus);
+
+    List<DataValue> dataValues = new ArrayList<>(dtos.size());
+
+    for (StockLevelDTO dto : dtos) {
+
+      DataValue dataValue = new DataValue();
+      dataValue.setVenue(dto.getLocation());
+      dataValue.setValue(dto.getCount());
+
+      Cohort componentTypeCohort = new Cohort();
+      componentTypeCohort.setCategory(CohortConstants.COMPONENT_TYPE_CATEGORY);
+      componentTypeCohort.setComparator(Comparator.EQUALS);
+      componentTypeCohort.setOption(dto.getComponentType().getComponentTypeName());
+      dataValue.addCohort(componentTypeCohort);
 
       Cohort bloodTypeCohort = new Cohort();
       bloodTypeCohort.setCategory(CohortConstants.BLOOD_TYPE_CATEGORY);
