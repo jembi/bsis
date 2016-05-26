98d81de677cddf016229e17026581a1b5b5f28f8
Daniel Futerman <daniel@jembi.org>
2016-05-26 21:02:42 +0200

    BSIS-117 Find BloodTestResults in Date Range query
    Added a generic BloodTestResults in Date Range query - this doesn’t
    specifically look for POS results - i.e. it can be used in the TTI
    Prevalence report, but can also be re-used for other purposes (e.g.
    finding NEG results, total samples tested, comparison of NEG vs POS
    results etc.)

--- src/repository/BloodTestResultNamedQueryConstants.java
+++ src/repository/BloodTestResultNamedQueryConstants.java
@@ -8,5 +8,16 @@ public class BloodTestResultNamedQueryConstants {
       "SELECT COUNT(btr) " +
           "FROM BloodTestResult btr " +
           "WHERE btr.donation.id = :donationId ";
-
+  
+  public static final String NAME_FIND_BLOOD_TEST_RESULT_VALUE_OBJECTS_FOR_DATE_RANGE =
+      "BloodTestResult.findBloodTestResultDtosForDateRange";
+  public static final String QUERY_FIND_BLOOD_TEST_RESULT_VALUE_OBJECTS_FOR_DATE_RANGE =
+      "SELECT NEW dto.BloodTestResultDTO(b.bloodTest, b.result, b.donation.donor.gender, b.donation.donor.venue, COUNT(b)) " +
+          "FROM BloodTestResult b " +
+          "WHERE b.donation.donationDate BETWEEN :startDate AND :endDate " +
+          "AND b.donation.isDeleted = :deleted " +
+          "AND b.bloodTest.bloodTestType = :bloodTestType " +
+          "AND b.result = :result " +
+          "GROUP BY b.donation.donor.venue, b.donation.donor.gender, b.bloodTest, b.result " +
+          "ORDER BY b.donation.donor.venue, b.donation.donor.gender, b.bloodTest, b.result";
 }
