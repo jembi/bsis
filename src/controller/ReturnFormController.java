e3fc0c679b227959ed6071a552a3994e5d6f2af3
Dagmar Timler <dagmar.timler@gmail.com>
2016-06-01 14:34:19 +0200

    BSIS-1101: Create ReturnFormBackingFormValidator
    Created a validator for ReturnFormBackingForm. Note that the validator
    enforces that the attribute 'status' is required. It is required for
    updates and will be overwritten by the CRUD service on create.

--- src/controller/ReturnFormController.java
+++ src/controller/ReturnFormController.java
@@ -3,18 +3,23 @@ package controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import backingform.ReturnFormBackingForm;
-import controllerservice.ReturnFormControllerService;
 import utils.PermissionConstants;
+import backingform.ReturnFormBackingForm;
+import backingform.validator.ReturnFormBackingFormValidator;
+import controllerservice.ReturnFormControllerService;
 
 @RestController
 @RequestMapping("returnforms")
@@ -22,10 +27,18 @@ public class ReturnFormController {
 
   @Autowired
   private ReturnFormControllerService returnFormControllerService;
+  
+  @Autowired
+  private ReturnFormBackingFormValidator validator;
+  
+  @InitBinder
+  protected void initBinder(WebDataBinder binder) {
+    binder.setValidator(validator);
+  }
 
   @RequestMapping(method = RequestMethod.POST)
   @PreAuthorize("hasRole('" + PermissionConstants.ADD_ORDER_FORM + "')")
-  public ResponseEntity<Map<String, Object>> addReturnForm(@RequestBody ReturnFormBackingForm backingForm) {
+  public ResponseEntity<Map<String, Object>> addReturnForm(@Valid @RequestBody ReturnFormBackingForm backingForm) {
     Map<String, Object> map = new HashMap<>();
     map.put("returnForm", returnFormControllerService.createReturnForm(backingForm));
     return new ResponseEntity<>(map, HttpStatus.CREATED);
