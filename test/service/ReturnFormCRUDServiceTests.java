e21ac38d4b8ad1553d04126c4963b2700b7d8858
lauravignoli <vignoli.laura@gmail.com>
2016-06-01 10:45:06 +0200

    BSIS-1010 Added new package controllerservice to applicationContextTest.xml and improved tests.

--- test/service/ReturnFormCRUDServiceTests.java
+++ test/service/ReturnFormCRUDServiceTests.java
@@ -1,33 +1,51 @@
 package service;
 
-import org.junit.Assert;
+import static org.mockito.Mockito.verify;
+
+import java.util.Date;
+
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import helpers.builders.LocationBuilder;
 import helpers.builders.ReturnFormBuilder;
+import model.location.Location;
 import model.returnform.ReturnForm;
+import model.returnform.ReturnStatus;
+import repository.ReturnFormRepository;
 import suites.UnitTestSuite;
 
 public class ReturnFormCRUDServiceTests extends UnitTestSuite {
   
-  private static final Long ORDER_FORM_ID = 17L;
-  
   @InjectMocks
   private ReturnFormCRUDService returnFormCRUDService;
   
+  @Mock
+  private ReturnFormRepository returnFormRepository;
+
   @Test
   public void testCreateReturnForm_shouldPersist() {
     // Set up
-    ReturnForm returnForm = ReturnFormBuilder.aReturnForm().withId(null).build();
+    Location returnedFrom = LocationBuilder.aUsageSite().withId(1L).build();
+    Location returnedTo = LocationBuilder.aDistributionSite().withId(2L).build();
+    Date returnDate = new Date();
 
+    ReturnForm returnForm = ReturnFormBuilder.aReturnForm()
+        .withReturnedFrom(returnedFrom)
+        .withReturnedTo(returnedTo)
+        .withReturnDate(returnDate)
+        .withReturnStatus(null)
+        .build();
+    
+    ReturnForm returnFormToPersist = returnForm;
+    returnFormToPersist.setStatus(ReturnStatus.CREATED);
+    
     // Run test
     returnFormCRUDService.createReturnForm(returnForm);
-    
+
     // Verify
-    Assert.assertNotNull("Return form has been created", returnForm.getId());
-
+    verify(returnFormRepository).save(returnFormToPersist);
   }
-  
-
 
 }
