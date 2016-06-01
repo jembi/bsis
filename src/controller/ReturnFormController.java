78a5c712db6528a67df7c34105557190dcd3a63e
Dagmar Timler <dagmar.timler@gmail.com>
2016-06-01 14:40:49 +0200

    BSIS-1015: Create returnforms/form endpoint
    Created the forms endpoint for ReturnForm that returns a list of
    usages sites and a list of distribution sites. These lists will
    be displayed in the frontend when creating a ReturnForm.

--- src/controller/ReturnFormController.java
+++ src/controller/ReturnFormController.java
@@ -1,9 +1,12 @@
 package controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
+
+import model.location.Location;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,10 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import repository.LocationRepository;
 import utils.PermissionConstants;
 import backingform.ReturnFormBackingForm;
 import backingform.validator.ReturnFormBackingFormValidator;
 import controllerservice.ReturnFormControllerService;
+import factory.LocationViewModelFactory;
 
 @RestController
 @RequestMapping("returnforms")
@@ -31,9 +36,28 @@ public class ReturnFormController {
   @Autowired
   private ReturnFormBackingFormValidator validator;
   
+  @Autowired
+  private LocationRepository locationRepository;
+  
+  @Autowired
+  private LocationViewModelFactory locationViewModelFactory;
+  
   @InitBinder
   protected void initBinder(WebDataBinder binder) {
     binder.setValidator(validator);
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/form")
+  @PreAuthorize("hasRole('" + PermissionConstants.ADD_ORDER_FORM + "')")
+  public ResponseEntity<Map<String, Object>> getOrderFormForm() {
+    List<Location> usageSites = locationRepository.getUsageSites();
+    List<Location> distributionSites = locationRepository.getDistributionSites();
+
+    Map<String, Object> map = new HashMap<>();
+    map.put("returnForm", new ReturnFormBackingForm());
+    map.put("usageSites", locationViewModelFactory.createLocationViewModels(usageSites));
+    map.put("distributionSites", locationViewModelFactory.createLocationViewModels(distributionSites));
+    return new ResponseEntity<>(map, HttpStatus.OK);
   }
 
   @RequestMapping(method = RequestMethod.POST)
