aa056cfd4ac0cfca18a1dbb6f3f60a7b2f623a35
Brett Ausmeier <brett.ausmeier@jembi.org>
2016-06-15 11:40:25 +0200

    BSIS-1149 Add canDiscard constraint check to return form
    Only allow return forms to be discarded if they have been returned and
    they have at least one component which can be discarded.

--- src/service/ReturnFormConstraintChecker.java
+++ src/service/ReturnFormConstraintChecker.java
@@ -1,8 +1,11 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import controller.ComponentConstraintChecker;
+import model.component.Component;
 import model.returnform.ReturnForm;
 import model.returnform.ReturnStatus;
 
@@ -10,11 +13,31 @@ import model.returnform.ReturnStatus;
 @Service
 public class ReturnFormConstraintChecker {
   
+  @Autowired
+  private ComponentConstraintChecker componentConstraintChecker;
+  
   public boolean canEdit(ReturnForm returnForm) {
     return returnForm.getStatus() == ReturnStatus.CREATED;
   }
   
   public boolean canReturn(ReturnForm returnForm) {
     return returnForm.getStatus() == ReturnStatus.CREATED && !returnForm.getComponents().isEmpty();
+  }
+  
+  public boolean canDiscard(ReturnForm returnForm) {
+
+    if (returnForm.getStatus() != ReturnStatus.RETURNED) {
+      // Can't discard components for a return form which has not been returned
+      return false;
+    }
+
+    for (Component component : returnForm.getComponents()) {
+      if (componentConstraintChecker.canDiscard(component)) {
+        // If any components can be discarded then so can the return form
+        return true;
+      }
+    }
+
+    return false;
   }
 }
--- test/service/ReturnFormConstraintCheckerTests.java
+++ test/service/ReturnFormConstraintCheckerTests.java
@@ -4,13 +4,17 @@ import static helpers.builders.ComponentBuilder.aComponent;
 import static helpers.builders.ReturnFormBuilder.aReturnForm;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 
 import java.util.Collections;
 
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import controller.ComponentConstraintChecker;
 import model.component.Component;
+import model.component.ComponentStatus;
 import model.returnform.ReturnForm;
 import model.returnform.ReturnStatus;
 import suites.UnitTestSuite;
@@ -19,6 +23,8 @@ public class ReturnFormConstraintCheckerTests extends UnitTestSuite {
   
   @InjectMocks
   private ReturnFormConstraintChecker returnFormConstraintChecker;
+  @Mock
+  private ComponentConstraintChecker componentConstraintChecker;
   
   @Test
   public void testCanEditWithCreatedReturnForm_shouldReturnTrue() {
@@ -87,6 +93,57 @@ public class ReturnFormConstraintCheckerTests extends UnitTestSuite {
     
     // Verify
     assertThat(canReturn, is(true));
+  }
+  
+  @Test
+  public void testCanDiscardWithCreatedReturnForm_shouldReturnFalse() {
+    // Set up
+    ReturnForm returnForm = aReturnForm()
+        .withReturnStatus(ReturnStatus.CREATED)
+        .withComponent(aComponent().build())
+        .build();
+    
+    // Test
+    boolean canDiscardComponents = returnFormConstraintChecker.canDiscard(returnForm);
+    
+    // Verify
+    assertThat(canDiscardComponents, is(false));
+  }
+  
+  @Test
+  public void testCanDiscardWithReturnedReturnFormWithDiscardedComponents_shouldReturnFalse() {
+    // Set up
+    ReturnForm returnForm = aReturnForm()
+        .withReturnStatus(ReturnStatus.RETURNED)
+        .withComponent(aComponent().withStatus(ComponentStatus.DISCARDED).build())
+        .withComponent(aComponent().withStatus(ComponentStatus.DISCARDED).build())
+        .build();
+    
+    // Test
+    boolean canDiscardComponents = returnFormConstraintChecker.canDiscard(returnForm);
+    
+    // Verify
+    assertThat(canDiscardComponents, is(false));
+  }
+  
+  @Test
+  public void testCanDiscardWithReturnedReturnFormWithDiscardableComponents_shouldReturnTrue() {
+    // Set up
+    Component componentThatCanBeDiscarded = aComponent().withId(8889L).withStatus(ComponentStatus.AVAILABLE).build();
+    
+    ReturnForm returnForm = aReturnForm()
+        .withReturnStatus(ReturnStatus.RETURNED)
+        .withComponent(aComponent().withId(77L).withStatus(ComponentStatus.DISCARDED).build())
+        .withComponent(componentThatCanBeDiscarded)
+        .build();
+    
+    when(componentConstraintChecker.canDiscard(componentThatCanBeDiscarded)).thenReturn(true);
+    
+    // Test
+    boolean canDiscardComponents = returnFormConstraintChecker.canDiscard(returnForm);
+    
+    // Verify
+    assertThat(canDiscardComponents, is(true));
   }
 
 }
