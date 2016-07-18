41414971ef2df76a466afc016a239984fcd92790
Dagmar Timler <dagmar.timler@gmail.com>
2016-07-18 09:02:48 +0200

    BSIS-1321: Added method in LocationType to resolve enum from a Location instance

--- src/main/java/org/jembi/bsis/model/location/LocationType.java
+++ src/main/java/org/jembi/bsis/model/location/LocationType.java
@@ -3,4 +3,23 @@ package org.jembi.bsis.model.location;
 public enum LocationType {
 
   VENUE, PROCESSING_SITE, DISTRIBUTION_SITE, USAGE_SITE, TESTING_SITE;
+  
+  public static LocationType resolveLocationType(Location location) {
+    if (location.getIsVenue()) {
+      return VENUE;
+    }
+    if (location.getIsProcessingSite()) {
+      return PROCESSING_SITE;
+    }
+    if (location.getIsDistributionSite()) {
+      return DISTRIBUTION_SITE;
+    }
+    if (location.getIsUsageSite()) {
+      return USAGE_SITE;
+    }
+    if (location.getIsTestingSite()) {
+      return TESTING_SITE;
+    }
+    else throw new IllegalArgumentException("Unknown LocationType for location " + location);
+  }
 }
