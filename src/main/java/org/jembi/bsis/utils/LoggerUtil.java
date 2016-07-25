bc5362d789f03be3f9319087beacfab482406062
Brett Ausmeier <brett.ausmeier@jembi.org>
2016-07-25 09:48:33 +0200

    BSIS-1375 Replace custom log level with standard logging
    Remove BSIS_CLICK_LOG and replace usages with debug logging.

--- src/main/java/org/jembi/bsis/utils/LoggerUtil.java
+++ src/main/java/org/jembi/bsis/utils/LoggerUtil.java
@@ -1,26 +1,9 @@
 package org.jembi.bsis.utils;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.jembi.bsis.log4j.BsisLogLevel;
 
 public class LoggerUtil {
-  static final Logger logger = Logger.getLogger("bsis");
-
-  static {
-    // PropertyConfigurator.configure("classes/log4j.properties");
-  }
-
-  public static void logUrl(HttpServletRequest request) {
-    String urlString = request.getRequestURL().toString();
-    String queryString = request.getQueryString();
-    urlString += queryString != null && queryString.length() > 0 ? "?"
-        + queryString : "";
-    logger.log(BsisLogLevel.BSIS, urlString);
-  }
 
   public static void setLogLevel(String level) {
     Level logLevel;
