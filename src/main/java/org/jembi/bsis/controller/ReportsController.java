54a74cdcb5c4b8626c53e4a74fc122ccb222e128
lauravignoli <vignoli.laura@gmail.com>
2016-08-08 17:12:54 +0200

    BSIS-1515 Refactor ReportsController
    Move getDistributionSites method to ReportsControllerService.

--- src/main/java/org/jembi/bsis/controller/ReportsController.java
+++ src/main/java/org/jembi/bsis/controller/ReportsController.java
@@ -2,15 +2,11 @@ package org.jembi.bsis.controller;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.jembi.bsis.controllerservice.ReportsControllerService;
-import org.jembi.bsis.factory.LocationFactory;
 import org.jembi.bsis.model.inventory.InventoryStatus;
-import org.jembi.bsis.model.location.Location;
 import org.jembi.bsis.model.reporting.Report;
-import org.jembi.bsis.repository.LocationRepository;
 import org.jembi.bsis.service.ReportGeneratorService;
 import org.jembi.bsis.service.TtiPrevalenceReportGeneratorService;
 import org.jembi.bsis.utils.PermissionConstants;
@@ -27,14 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportsController {
 
   @Autowired
-  private LocationRepository locationRepository;
-
-  @Autowired
   private ReportGeneratorService reportGeneratorService;
   
-  @Autowired
-  private LocationFactory locationFactory;
-
   @Autowired
   private TtiPrevalenceReportGeneratorService ttiPrevalenceReportGeneratorService;
 
@@ -51,10 +41,8 @@ public class ReportsController {
   @RequestMapping(value = "/stockLevels/form", method = RequestMethod.GET)
   @PreAuthorize("hasRole('" + PermissionConstants.VIEW_INVENTORY_INFORMATION + "')")
   public Map<String, Object> stockLevelsFormGenerator() {
-    List<Location> distributionSites = locationRepository.getDistributionSites();
-
     Map<String, Object> map = new HashMap<>();
-    map.put("distributionSites", locationFactory.createFullViewModels(distributionSites));
+    map.put("distributionSites", reportsControllerService.getDistributionSites());
     return map;
   }
 
--- src/main/java/org/jembi/bsis/controllerservice/ReportsControllerService.java
+++ src/main/java/org/jembi/bsis/controllerservice/ReportsControllerService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.jembi.bsis.factory.ComponentTypeFactory;
+import org.jembi.bsis.factory.LocationFactory;
 import org.jembi.bsis.repository.ComponentTypeRepository;
+import org.jembi.bsis.repository.LocationRepository;
 import org.jembi.bsis.viewmodel.ComponentTypeViewModel;
+import org.jembi.bsis.viewmodel.LocationViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +23,18 @@ public class ReportsControllerService {
   @Autowired
   private ComponentTypeFactory componentTypeFactory;
 
+  @Autowired
+  private LocationRepository locationRepository;
+
+  @Autowired
+  private LocationFactory locationFactory;
+
   public List<ComponentTypeViewModel> getAllComponentTypesThatCanBeIssued() {
     return componentTypeFactory.createViewModels(componentTypeRepository.getAllComponentTypesThatCanBeIssued());
   }
   
+  public List<LocationViewModel> getDistributionSites() {
+    return locationFactory.createViewModels(locationRepository.getDistributionSites());
+  }
+
 }
