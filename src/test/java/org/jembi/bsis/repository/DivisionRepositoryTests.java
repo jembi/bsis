69c9a63658441f5fc37ebfd7cd030e0c9045cfec
rowan84 <rowan.pillay@jembi.org>
2016-08-22 13:41:11 +0200

    BSIS-1582: DonorRepository Testcase clean up and improvements
    Add testcases for blank and null name and null level. Also corrected
    erroneous comments and assertion messages on the existing testcases.

--- src/test/java/org/jembi/bsis/repository/DivisionRepositoryTests.java
+++ src/test/java/org/jembi/bsis/repository/DivisionRepositoryTests.java
@@ -16,129 +16,171 @@ public class DivisionRepositoryTests extends ContextDependentTestSuite {
   private DivisionRepository divisionRepository;
   
   @Test
-  public void testFindDivisionByExactName_verifyCorrectDivisionReturned() {
-    Division division1 = aDivision().withName("division1").buildAndPersist(entityManager); // match
+  public void testFindDivisionByExactNameAndNullLevel_verifyCorrectDivisionReturned() {
+    Division division1 = aDivision().withName("division1").buildAndPersist(entityManager); // match 
     Division division2 = aDivision().withName("division2").buildAndPersist(entityManager);
-    Division division3 = aDivision().withName("division3").buildAndPersist(entityManager); // match
+    Division division3 = aDivision().withName("division3").buildAndPersist(entityManager); 
     Division division4 = aDivision().withName("division4").buildAndPersist(entityManager);
-    Division division5 = aDivision().withName("division5").buildAndPersist(entityManager); // match
+    Division division5 = aDivision().withName("division5").buildAndPersist(entityManager); 
    
     List<Division> divisions = divisionRepository.findDivisions("division1", false, null);
     
     // Verify only 1 division returned
     Assert.assertEquals("Verify divisions returned", 1, divisions.size());
 
-    // Verify right divions were returned
-    Assert.assertTrue("Verify locations", divisions.contains(division1));
-    Assert.assertFalse("Verify locations", divisions.contains(division2));
-    Assert.assertFalse("Verify locations", divisions.contains(division3));
-    Assert.assertFalse("Verify locations", divisions.contains(division4));
-    Assert.assertFalse("Verify locations", divisions.contains(division5));
+    // Verify right divisions were returned
+    Assert.assertTrue("Verify division 1 present", divisions.contains(division1));
+    Assert.assertFalse("Verify division 2 absent", divisions.contains(division2));
+    Assert.assertFalse("Verify division 3 absent", divisions.contains(division3));
+    Assert.assertFalse("Verify division 4 absent", divisions.contains(division4));
+    Assert.assertFalse("Verify division 5 absent", divisions.contains(division5));
   }
 
   @Test
-  public void testFindDivisionBySimilarName_verifyCorrectDivisionsReturned() {
+  public void testFindDivisionBySimilarNameAndNullLevel_verifyCorrectDivisionsReturned() {
     Division division1 = aDivision().withName("division1").buildAndPersist(entityManager); // match
-    Division division2 = aDivision().withName("division2").buildAndPersist(entityManager);
+    Division division2 = aDivision().withName("division2").buildAndPersist(entityManager); // match
     Division division3 = aDivision().withName("division3").buildAndPersist(entityManager); // match
-    Division division4 = aDivision().withName("division4").buildAndPersist(entityManager);
-    Division division5 = aDivision().withName("test").buildAndPersist(entityManager); // match
+    Division division4 = aDivision().withName("division4").buildAndPersist(entityManager); // match
+    Division division5 = aDivision().withName("test").buildAndPersist(entityManager); 
    
     List<Division> divisions = divisionRepository.findDivisions("division", true, null);
     
     // Verify only 4 divisions are returned
     Assert.assertEquals("Verify divisions returned", 4, divisions.size());
 
-    // Verify right divions were returned
-    Assert.assertTrue("Verify locations", divisions.contains(division1));
-    Assert.assertTrue("Verify locations", divisions.contains(division2));
-    Assert.assertTrue("Verify locations", divisions.contains(division3));
-    Assert.assertTrue("Verify locations", divisions.contains(division4));
-    Assert.assertFalse("Verify locations", divisions.contains(division5));
+    // Verify right divisions were returned
+    Assert.assertTrue("Verify division 1 present", divisions.contains(division1));
+    Assert.assertTrue("Verify division 2 present", divisions.contains(division2));
+    Assert.assertTrue("Verify division 3 present", divisions.contains(division3));
+    Assert.assertTrue("Verify division 4 present", divisions.contains(division4));
+    Assert.assertFalse("Verify division 5 absent", divisions.contains(division5));
   }
   
   @Test
   public void testFindDivisionBySimilarNameAndLevelType_verifyCorrectDivisionsReturned() {
-    Division division1 = aDivision().withName("division1").withLevel(1).buildAndPersist(entityManager); // match
-    Division division2 = aDivision().withName("division2").withLevel(1).buildAndPersist(entityManager);
-    Division division3 = aDivision().withName("division3").withLevel(2).buildAndPersist(entityManager); // match
-    Division division4 = aDivision().withName("division4").withLevel(2).buildAndPersist(entityManager);
-    Division division5 = aDivision().withName("test").withLevel(1).buildAndPersist(entityManager); // match
-   
-    List<Division> divisions = divisionRepository.findDivisions("division", true, 2);
-    
-    // Verify only 2 divisions are returned
-    Assert.assertEquals("Verify divisions returned", 2, divisions.size());
-
-    // Verify right divions were returned
-    Assert.assertFalse("Verify locations", divisions.contains(division1));
-    Assert.assertFalse("Verify locations", divisions.contains(division2));
-    Assert.assertTrue("Verify locations", divisions.contains(division3));
-    Assert.assertTrue("Verify locations", divisions.contains(division4));
-    Assert.assertFalse("Verify locations", divisions.contains(division5));
-  }
-  
-  @Test
-  public void testFindDivisionWithParentBySimilarNameAndLevel_2_verifyCorrectDivisionsReturned() {
-    Division division1 = aDivision().withName("division1").withLevel(1).buildAndPersist(entityManager); // match
+    Division division1 = aDivision().withName("division1").withLevel(1).buildAndPersist(entityManager);
     Division division2 = aDivision().withName("division2").withLevel(1).buildAndPersist(entityManager);
     Division division3 = aDivision().withName("division3").withParent(division1).withLevel(2).buildAndPersist(entityManager); // match
-    Division division4 = aDivision().withName("division4").withParent(division1).withLevel(2).buildAndPersist(entityManager);
-    Division division5 = aDivision().withName("test").withLevel(1).buildAndPersist(entityManager); // match
+    Division division4 = aDivision().withName("division4").withParent(division1).withLevel(2).buildAndPersist(entityManager); // match
+    Division division5 = aDivision().withName("test").withLevel(1).buildAndPersist(entityManager); 
    
     List<Division> divisions = divisionRepository.findDivisions("division", true, 2);
     
     // Verify only 2 divisions are returned
     Assert.assertEquals("Verify divisions returned", 2, divisions.size());
 
-    // Verify right divions were returned
-    Assert.assertFalse("Verify locations", divisions.contains(division1));
-    Assert.assertFalse("Verify locations", divisions.contains(division2));
-    Assert.assertTrue("Verify locations", divisions.contains(division3));
-    Assert.assertTrue("Verify locations", divisions.contains(division4));
-    Assert.assertFalse("Verify locations", divisions.contains(division5));
+    // Verify right divisions were returned
+    Assert.assertFalse("Verify division 1 absent", divisions.contains(division1));
+    Assert.assertFalse("Verify division 2 absent", divisions.contains(division2));
+    Assert.assertTrue("Verify division 3 present", divisions.contains(division3));
+    Assert.assertTrue("Verify division 4 present", divisions.contains(division4));
+    Assert.assertFalse("Verify division 5 absent", divisions.contains(division5));
   }
-  
+    
   @Test
   public void testFindDivisionBySimilarNameAndIncorrectLevelType_verifyNoDivisionsReturned() {
-    Division division1 = aDivision().withName("division1").withLevel(1).buildAndPersist(entityManager); // match
+    Division division1 = aDivision().withName("division1").withLevel(1).buildAndPersist(entityManager); 
     Division division2 = aDivision().withName("division2").withLevel(1).buildAndPersist(entityManager);
-    Division division3 = aDivision().withName("division3").withLevel(2).buildAndPersist(entityManager); // match
-    Division division4 = aDivision().withName("division4").withLevel(2).buildAndPersist(entityManager);
-    Division division5 = aDivision().withName("test").withLevel(1).buildAndPersist(entityManager); // match
+    Division division3 = aDivision().withName("division3").withParent(division1).withLevel(2).buildAndPersist(entityManager); 
+    Division division4 = aDivision().withName("division4").withParent(division1).withLevel(2).buildAndPersist(entityManager);
+    Division division5 = aDivision().withName("test").withLevel(1).buildAndPersist(entityManager);
    
     List<Division> divisions = divisionRepository.findDivisions("division", true, 3);
     
     // Verify no divisions are returned
     Assert.assertEquals("Verify divisions returned", 0, divisions.size());
 
-    // Verify right divions were returned
-    Assert.assertFalse("Verify locations", divisions.contains(division1));
-    Assert.assertFalse("Verify locations", divisions.contains(division2));
-    Assert.assertFalse("Verify locations", divisions.contains(division3));
-    Assert.assertFalse("Verify locations", divisions.contains(division4));
-    Assert.assertFalse("Verify locations", divisions.contains(division5));
+    // Verify right divisions were returned
+    Assert.assertFalse("Verify division 1 absent", divisions.contains(division1));
+    Assert.assertFalse("Verify division 2 absent", divisions.contains(division2));
+    Assert.assertFalse("Verify division 3 absent", divisions.contains(division3));
+    Assert.assertFalse("Verify division 4 absent", divisions.contains(division4));
+    Assert.assertFalse("Verify division 5 absent", divisions.contains(division5));
   }
   
   @Test
-  public void testFindDivisionBySimilarName_verifyNoDivisionsReturned() {
-    Division division1 = aDivision().withName("division1").buildAndPersist(entityManager); // match
+  public void testFindDivisionBySimilarNameAndNullLevel_verifyNoDivisionsReturned() {
+    Division division1 = aDivision().withName("division1").buildAndPersist(entityManager); 
     Division division2 = aDivision().withName("division2").buildAndPersist(entityManager);
-    Division division3 = aDivision().withName("division3").buildAndPersist(entityManager); // match
+    Division division3 = aDivision().withName("division3").buildAndPersist(entityManager); 
     Division division4 = aDivision().withName("division4").buildAndPersist(entityManager);
-    Division division5 = aDivision().withName("test").buildAndPersist(entityManager); // match
+    Division division5 = aDivision().withName("test").buildAndPersist(entityManager);
    
     List<Division> divisions = divisionRepository.findDivisions("tester", true, null);
     
     // Verify no divisions are returned
-    Assert.assertEquals("Verify locations returned", 0, divisions.size());
+    Assert.assertEquals("Verify divisions returned", 0, divisions.size());
 
-    // Verify right divions were returned
-    Assert.assertFalse("Verify locations", divisions.contains(division1));
-    Assert.assertFalse("Verify locations", divisions.contains(division2));
-    Assert.assertFalse("Verify locations", divisions.contains(division3));
-    Assert.assertFalse("Verify locations", divisions.contains(division4));
-    Assert.assertFalse("Verify locations", divisions.contains(division5));
+    // Verify right divisions were returned
+    Assert.assertFalse("Verify division 1 absent", divisions.contains(division1));
+    Assert.assertFalse("Verify division 2 absent", divisions.contains(division2));
+    Assert.assertFalse("Verify division 3 absent", divisions.contains(division3));
+    Assert.assertFalse("Verify division 4 absent", divisions.contains(division4));
+    Assert.assertFalse("Verify division 5 absent", divisions.contains(division5));
+  }
+  
+  @Test
+  public void testFindDivisionWithBlankNameAndNullLevel_verifyAllDivisionsReturned() {
+    Division division1 = aDivision().withName("division1").buildAndPersist(entityManager);  // match 
+    Division division2 = aDivision().withName("division2").buildAndPersist(entityManager);  // match
+    Division division3 = aDivision().withName("division3").buildAndPersist(entityManager);  // match
+    Division division4 = aDivision().withName("division4").buildAndPersist(entityManager);  // match
+    Division division5 = aDivision().withName("test").buildAndPersist(entityManager);       // match
+   
+    List<Division> divisions = divisionRepository.findDivisions("", false, null);
+    
+    // Verify all divisions are returned
+    Assert.assertEquals("Verify all divisions returned", 5, divisions.size());
+
+    // Verify right divisions were returned
+    Assert.assertTrue("Verify division 1 present", divisions.contains(division1));
+    Assert.assertTrue("Verify division 2 present", divisions.contains(division2));
+    Assert.assertTrue("Verify division 3 present", divisions.contains(division3));
+    Assert.assertTrue("Verify division 4 present", divisions.contains(division4));
+    Assert.assertTrue("Verify division 5 present", divisions.contains(division5));
+  }
+
+  @Test
+  public void testFindDivisionWithNullNameAndNullLevel_verifyAllDivisionsReturned() {
+    Division division1 = aDivision().withName("division1").buildAndPersist(entityManager); // match  
+    Division division2 = aDivision().withName("division2").buildAndPersist(entityManager); // match
+    Division division3 = aDivision().withName("division3").buildAndPersist(entityManager); // match
+    Division division4 = aDivision().withName("division4").buildAndPersist(entityManager); // match
+    Division division5 = aDivision().withName("test").buildAndPersist(entityManager);      // match
+   
+    List<Division> divisions = divisionRepository.findDivisions(null, false, null);
+    
+    // Verify all divisions are returned
+    Assert.assertEquals("Verify all divisions returned", 5, divisions.size());
+
+    // Verify right divisions were returned
+    Assert.assertTrue("Verify division 1 present", divisions.contains(division1));
+    Assert.assertTrue("Verify division 2 present", divisions.contains(division2));
+    Assert.assertTrue("Verify division 3 present", divisions.contains(division3));
+    Assert.assertTrue("Verify division 4 present", divisions.contains(division4));
+    Assert.assertTrue("Verify division 5 present", divisions.contains(division5));
+  }
+  
+  @Test
+  public void testFindDivisionWithBlankNameAndLevel2_verifyCorrectDivisionsReturned() {
+    Division division1 = aDivision().withName("division1").buildAndPersist(entityManager);  
+    Division division2 = aDivision().withName("division2").buildAndPersist(entityManager);
+    Division division3 = aDivision().withName("division3").withLevel(2).buildAndPersist(entityManager); // match  
+    Division division4 = aDivision().withName("division4").withLevel(2).buildAndPersist(entityManager); // match  
+    Division division5 = aDivision().withName("test").withLevel(2).buildAndPersist(entityManager);      // match  
+   
+    List<Division> divisions = divisionRepository.findDivisions("", false, 2);
+    
+    // Verify correct amount of divisions are returned
+    Assert.assertEquals("Verify all divisions returned", 3, divisions.size());
+
+    // Verify right divisions were returned
+    Assert.assertFalse("Verify division 1 absent", divisions.contains(division1));
+    Assert.assertFalse("Verify division 2 absent", divisions.contains(division2));
+    Assert.assertTrue("Verify division 3 present", divisions.contains(division3));
+    Assert.assertTrue("Verify division 4 present", divisions.contains(division4));
+    Assert.assertTrue("Verify division 5 present", divisions.contains(division5));
   }
 
 }
