0f62f48549720375c7bc721a9374714b5146dac4
rowan84 <rowan.pillay@jembi.org>
2016-08-22 11:13:21 +0200

    BSIS-1582: Update DivisionControllerService to use actual DivisionRepository
    DivisionControllerService now calls the relevant method in the DivisionRepository to find Divisions.

--- src/main/java/org/jembi/bsis/controllerservice/DivisionControllerService.java
+++ src/main/java/org/jembi/bsis/controllerservice/DivisionControllerService.java
@@ -1,10 +1,10 @@
 package org.jembi.bsis.controllerservice;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.jembi.bsis.factory.DivisionFactory;
 import org.jembi.bsis.model.location.Division;
+import org.jembi.bsis.repository.DivisionRepository;
 import org.jembi.bsis.viewmodel.DivisionViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,9 +17,11 @@ public class DivisionControllerService {
   @Autowired
   private DivisionFactory divisionFactory;
   
+  @Autowired
+  private DivisionRepository divisionRepository;
+  
   public List<DivisionViewModel> findDivisions(String name, boolean includeSimilarResults, Integer level) {
-    // TODO: Use query to find location divisions
-    List<Division> divisions = Collections.<Division>emptyList();
+    List<Division> divisions = divisionRepository.findDivisions(name, includeSimilarResults, level);
     return divisionFactory.createDivisionViewModels(divisions);
   }
 
