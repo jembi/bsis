1295a47f0649e4e0d9952477ed1b0818b667e696
rowan84 <rowan.pillay@jembi.org>
2016-08-24 14:18:40 +0200

    BSIS-1544: Add JUnit tests for DivisionFactory createEntity
    Add tests to DivisionFactoryTests.java to test conversion from
    DivisionBackingForm to Division Entity. 2 Tests are added i.e.
    one without a parent division entity and one with a parent division entity.

--- src/test/java/org/jembi/bsis/factory/DivisionFactoryTests.java
+++ src/test/java/org/jembi/bsis/factory/DivisionFactoryTests.java
@@ -2,19 +2,25 @@ package org.jembi.bsis.factory;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.jembi.bsis.helpers.builders.DivisionBackingFormBuilder.aDivisionBackingForm;
 import static org.jembi.bsis.helpers.builders.DivisionBuilder.aDivision;
 import static org.jembi.bsis.helpers.builders.DivisionViewModelBuilder.aDivisionViewModel;
 import static org.jembi.bsis.helpers.matchers.DivisionViewModelMatcher.hasSameStateAsDivisionViewModel;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.jembi.bsis.backingform.DivisionBackingForm;
 import org.jembi.bsis.model.location.Division;
+import org.jembi.bsis.repository.DivisionRepository;
 import org.jembi.bsis.suites.UnitTestSuite;
 import org.jembi.bsis.viewmodel.DivisionViewModel;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
 
 public class DivisionFactoryTests extends UnitTestSuite {
@@ -22,6 +28,9 @@ public class DivisionFactoryTests extends UnitTestSuite {
   @Spy
   @InjectMocks
   private DivisionFactory divisionFactory;
+  @Mock
+  private DivisionRepository divisionRepository;
+
   
   @Test
   public void testCreateDivisionViewModel_shouldReturnViewModelWithTheCorrectState() {
@@ -147,5 +156,73 @@ public class DivisionFactoryTests extends UnitTestSuite {
     // Verify
     assertThat(returnedViewModels, is(expectedViewModels));
   }
+  
+  @Test
+  public void testConvertDivisionBackingFormToDivisionEntity_shouldReturnExpectedEntity() {    
+    long id = 5L;
+    String name = "aDiv";
+    int level = 1;
+    DivisionBackingForm form = aDivisionBackingForm()
+        .withId(id)
+        .withName(name)
+        .withLevel(level)
+        .build();
+
+    Division convertedEntity = divisionFactory.createEntity(form);
+
+    Assert.assertNotNull("Entity was created", convertedEntity);
+    Assert.assertEquals("Entity id is correct", id, form.getId());
+    Assert.assertEquals("Entity name is correct", name, form.getName());
+    Assert.assertEquals("Entity level is correct", level, form.getLevel());
+    Assert.assertNull("Entity parent is null", form.getParent());
+    
+  }
+
+  @Test
+  public void testConvertDivisionBackingFormWithParentToDivisionEntityWithParent_shouldReturnExpectedEntity() {
+    long parentId = 4L;
+    long id = 5L;
+
+    String parentName = "parentName";
+    String name = "aDiv";
+
+    int parentLevel = 1;
+    int level = 2;
+
+    Division parent = aDivision()
+        .withLevel(parentLevel)
+        .withName(parentName)
+        .build();
+
+    DivisionBackingForm parentForm = aDivisionBackingForm()
+        .withId(parentId)
+        .withName(parentName)
+        .withLevel(parentLevel)
+        .build();
+
+    DivisionBackingForm form = aDivisionBackingForm()
+        .withId(id)
+        .withName(name)
+        .withLevel(level)
+        .withParent(parentForm)
+        .build();
+
+    when(divisionRepository.findDivisionById(4L)).thenReturn(parent);
+
+    Division convertedEntity = divisionFactory.createEntity(form);
+
+    Assert.assertNotNull("Entity was created", convertedEntity);
+    Assert.assertEquals("Entity id is correct",  Long.valueOf(id), convertedEntity.getId());
+    Assert.assertEquals("Entity name is correct", name, convertedEntity.getName());
+    Assert.assertEquals("Entity level is correct", level, convertedEntity.getLevel());
+
+    Division convertedEntityParent = convertedEntity.getParent();
+
+    Assert.assertNotNull("Entity Parent was set", convertedEntityParent);
+    Assert.assertEquals("Entity parent name is correct", parentName, convertedEntityParent.getName());
+    Assert.assertEquals("Entity parent level is correct", parentLevel, convertedEntityParent.getLevel());
+    Assert.assertNull("Entity parent's parent is not set", convertedEntityParent.getParent());
+  }
 
 }
+
