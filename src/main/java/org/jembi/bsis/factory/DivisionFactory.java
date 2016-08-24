d7b81dc4c03f76735f5c5035da4add6b76ec3007
rowan84 <rowan.pillay@jembi.org>
2016-08-24 14:18:11 +0200

    BSIS-1544: Add createEntity method to DivisionFactory.java
    Add a create entity method to the DivisionFactory class.

--- src/main/java/org/jembi/bsis/factory/DivisionFactory.java
+++ src/main/java/org/jembi/bsis/factory/DivisionFactory.java
@@ -3,15 +3,21 @@ package org.jembi.bsis.factory;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jembi.bsis.backingform.DivisionBackingForm;
 import org.jembi.bsis.model.location.Division;
+import org.jembi.bsis.repository.DivisionRepository;
 import org.jembi.bsis.viewmodel.DivisionViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 @Service
 public class DivisionFactory {
-  
+
+  @Autowired
+  private DivisionRepository divisionRepository;
+
   public DivisionViewModel createDivisionViewModel(Division division) {
     return createDivisionViewModel(division, true);
   }
@@ -27,7 +33,7 @@ public class DivisionFactory {
     }
     return viewModel;
   }
-  
+
   public List<DivisionViewModel> createDivisionViewModels(List<Division> divisions) {
     List<DivisionViewModel> viewModels = new ArrayList<>();
     for (Division division : divisions) {
@@ -36,4 +42,20 @@ public class DivisionFactory {
     return viewModels;
   }
 
+  public Division createEntity(DivisionBackingForm form) {
+    Division division = new Division();
+    division.setId(form.getId());
+    division.setName(form.getName());
+    division.setLevel(form.getLevel());
+
+    DivisionBackingForm parentBackingForm = form.getParent();
+    if(parentBackingForm != null) {
+      Division parent = divisionRepository.findDivisionById(parentBackingForm.getId());
+      division.setParent(parent);
+    }
+
+    return division;
+  }
+
 }
+
