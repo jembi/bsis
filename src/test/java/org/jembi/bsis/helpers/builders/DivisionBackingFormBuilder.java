d69bdd8bb28b98b087c5aca1dc6a50e17a2b3307
lauravignoli <vignoli.laura@gmail.com>
2016-08-24 13:34:17 +0200

    BSIS-1538 Add DivisionBackingFormBuilder

---
+++ src/test/java/org/jembi/bsis/helpers/builders/DivisionBackingFormBuilder.java
@@ -0,0 +1,46 @@
+package org.jembi.bsis.helpers.builders;
+
+import org.jembi.bsis.backingform.DivisionBackingForm;
+
+public class DivisionBackingFormBuilder extends AbstractBuilder<DivisionBackingForm> {
+  
+  private long id;
+  private String name;
+  private int level;
+  private DivisionBackingForm parent;
+  
+  public DivisionBackingFormBuilder withId(long id) {
+    this.id = id;
+    return this;
+  }
+
+  public DivisionBackingFormBuilder withName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public DivisionBackingFormBuilder withLevel(int level) {
+    this.level = level;
+    return this;
+  }
+  
+  public DivisionBackingFormBuilder withParent(DivisionBackingForm parent) {
+    this.parent = parent;
+    return this;
+  }
+
+  @Override
+  public DivisionBackingForm build() {
+    DivisionBackingForm form = new DivisionBackingForm();
+    form.setId(id);
+    form.setName(name);
+    form.setLevel(level);
+    form.setParent(parent);
+    return form;
+  }
+  
+  public static DivisionBackingFormBuilder aDivisionBackingForm() {
+    return new DivisionBackingFormBuilder();
+  }
+
+}
