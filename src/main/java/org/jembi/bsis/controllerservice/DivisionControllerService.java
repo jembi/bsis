c847bcbebf3a8636153b1facc488bf9574ac4ddb
rowan84 <rowan.pillay@jembi.org>
2016-08-25 12:18:27 +0200

    BSIS-1539: Add POST enpoint to DivisionController and DivisionControllerService
    Add POST endpoint to DivisionController and DivisionControllerService to allow addition
    of Divisions. Included in this commit is the linking of the Division Backing Form Validator
    for validation of user entered values.

--- src/main/java/org/jembi/bsis/controllerservice/DivisionControllerService.java
+++ src/main/java/org/jembi/bsis/controllerservice/DivisionControllerService.java
@@ -2,9 +2,11 @@ package org.jembi.bsis.controllerservice;
 
 import java.util.List;
 
+import org.jembi.bsis.backingform.DivisionBackingForm;
 import org.jembi.bsis.factory.DivisionFactory;
 import org.jembi.bsis.model.location.Division;
 import org.jembi.bsis.repository.DivisionRepository;
+import org.jembi.bsis.service.DivisionCRUDService;
 import org.jembi.bsis.viewmodel.DivisionViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,18 +15,26 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Service
 public class DivisionControllerService {
-  
+
   @Autowired
   private DivisionFactory divisionFactory;
-  
+
   @Autowired
   private DivisionRepository divisionRepository;
   
+  @Autowired
+  private DivisionCRUDService divisionCRUDService;
+
+  public DivisionViewModel createDivision(DivisionBackingForm form) {
+    Division createdDivision = divisionCRUDService.createDivision(divisionFactory.createEntity(form));
+    return divisionFactory.createDivisionViewModel(createdDivision);
+  }
+
   public List<DivisionViewModel> findDivisions(String name, boolean includeSimilarResults, Integer level) {
     List<Division> divisions = divisionRepository.findDivisions(name, includeSimilarResults, level);
     return divisionFactory.createDivisionViewModels(divisions);
   }
-  
+
   public DivisionViewModel findDivisionById(long id) {
     Division division = divisionRepository.findDivisionById(id);
     return divisionFactory.createDivisionViewModel(division);
