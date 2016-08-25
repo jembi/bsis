874a0782026236239a8f44cde8aa066bc5182e3b
Brett Ausmeier <brett.ausmeier@jembi.org>
2016-08-25 16:19:34 +0200

    BSIS-1540 Add test for updateDivision CRUD service method
    Test that the existing division is correctly updated.

--- src/test/java/org/jembi/bsis/service/DivisionCRUDServiceTests.java
+++ src/test/java/org/jembi/bsis/service/DivisionCRUDServiceTests.java
@@ -4,8 +4,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.jembi.bsis.helpers.builders.DivisionBuilder.aDivision;
 import static org.jembi.bsis.helpers.matchers.DivisionMatcher.hasSameStateAsDivision;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.jembi.bsis.model.location.Division;
 import org.jembi.bsis.repository.DivisionRepository;
@@ -42,5 +45,36 @@ public class DivisionCRUDServiceTests {
     verify(divisionRepository).save(argThat(hasSameStateAsDivision(expectedEntity)));
 
     assertThat(createdDivision, is(expectedEntity));
+  }
+  
+  @Test
+  public void testUpdateDivision_shouldSetCorrectFieldsAndUpdate() {
+    // Set up fixture
+    long divisionId = 877L;
+
+    Division existingDivision = aDivision()
+        .withId(divisionId)
+        .withName("Existing")
+        .withLevel(1)
+        .withParent(null)
+        .build();
+    
+    Division updatedDivision = aDivision()
+        .withId(divisionId)
+        .withName("Updated")
+        .withLevel(2)
+        .withParent(aDivision().withId(1003L).build())
+        .build();
+    
+    // Set up expectations
+    when(divisionRepository.findDivisionById(divisionId)).thenReturn(existingDivision);
+    when(divisionRepository.update(any(Division.class))).thenAnswer(returnsFirstArg());
+    
+    // Exercise SUT
+    Division returnedDivision = divisionCRUDService.updateDivision(updatedDivision);
+    
+    // Verify
+    verify(divisionRepository).update(argThat(hasSameStateAsDivision(updatedDivision)));
+    assertThat(returnedDivision, hasSameStateAsDivision(updatedDivision));
   }
 }
