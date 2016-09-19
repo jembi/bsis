32fba883e37a96c1667705196b28f814c7706e3d
tumijacob <matome.phoshoko@jembi.org>
2016-09-19 13:00:23 +0200

    BSIS-1718 Add new Builder and Matcher test files for ComponentTypeViewModel * Added new Builder and a matcher for ComponentTypeViewModel

---
+++ src/test/java/org/jembi/bsis/helpers/builders/ComponentTypeViewModelBuilder.java
@@ -0,0 +1,53 @@
+package org.jembi.bsis.helpers.builders;
+
+import org.jembi.bsis.viewmodel.ComponentTypeViewModel;
+
+public class ComponentTypeViewModelBuilder extends AbstractBuilder<ComponentTypeViewModel> {
+
+  private Long id;
+  private String componentTypeName;
+  private String componentTypeCode;
+  private String description;
+  private boolean containsPlasma;
+
+  public ComponentTypeViewModelBuilder withId(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public ComponentTypeViewModelBuilder withComponentTypeName(String componentTypeName) {
+    this.componentTypeName = componentTypeName;
+    return this;
+  }
+
+  public ComponentTypeViewModelBuilder withComponentTypeCode(String componentTypeCode) {
+    this.componentTypeCode = componentTypeCode;
+    return this;
+  }
+
+  public ComponentTypeViewModelBuilder withDescription(String description) {
+    this.description = description;
+    return this;
+  }
+
+  public ComponentTypeViewModelBuilder withContainsPlasma(boolean containsPlasma) {
+    this.containsPlasma = containsPlasma;
+    return this;
+  }
+
+  @Override
+  public ComponentTypeViewModel build() {
+    ComponentTypeViewModel viewModel = new ComponentTypeViewModel();
+    viewModel.setId(id);
+    viewModel.setComponentTypeName(componentTypeName);
+    viewModel.setComponentTypeCode(componentTypeCode);
+    viewModel.setDescription(description);
+    viewModel.setIsContainsPlasma(containsPlasma);
+
+    return viewModel;
+  }
+
+  public static ComponentTypeViewModelBuilder aComponentTypeViewModelBuilder() {
+    return new ComponentTypeViewModelBuilder();
+  }
+}
