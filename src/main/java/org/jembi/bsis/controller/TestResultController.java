555096229c2b16e786dedaea838a62e7884a60b2
Judge Muzinda <jmuzinda@gmail.com>
2016-10-07 11:27:00 +0200

    BSIS-1821 Remove numberOfDonations endpoint
    Remove numberOfDonations end point in controller as it is no longer being used anywhere.

--- src/main/java/org/jembi/bsis/controller/TestResultController.java
+++ src/main/java/org/jembi/bsis/controller/TestResultController.java
@@ -99,13 +99,11 @@ public class TestResultController {
       @RequestParam(value = "bloodTestType", required = false) BloodTestType bloodTestType) {
 
     Map<String, Object> map = new HashMap<String, Object>();
-    int numberOfDonations = 0;
     TestBatch testBatch = testBatchRepository.findTestBatchById(testBatchId);
     Set<DonationBatch> donationBatches = testBatch.getDonationBatches();
     List<Long> donationBatchIds = new ArrayList<Long>();
     for(DonationBatch donationBatch : donationBatches){
       donationBatchIds.add(donationBatch.getId());
-      numberOfDonations += donationBatch.getDonations().size();
     }
 
     List<BloodTestingRuleResult> ruleResults;
@@ -118,7 +116,6 @@ public class TestResultController {
 
     map.put("testResults", ruleResults);
     map.put("testBatchCreatedDate", CustomDateFormatter.format(testBatch.getCreatedDate()));
-    map.put("numberOfDonations", numberOfDonations);
 
     return new ResponseEntity<>(map, HttpStatus.OK);
   }
