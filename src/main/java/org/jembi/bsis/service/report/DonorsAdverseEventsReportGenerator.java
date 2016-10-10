16c04d1a27d480c633b40d828d5bac6038ed47c0
Dagmar Timler <dagmar.timler@gmail.com>
2016-10-10 13:32:32 +0200

    BSIS-1766 Add Generator for Adverse Events report
    Created ReportGenerator for the Donor Adverse Events Report which
    takes the DonorsAdverseEventsDTO and transforms them into a Report
    entity (with dataValues).

---
+++ src/main/java/org/jembi/bsis/service/report/DonorsAdverseEventsReportGenerator.java
@@ -0,0 +1,57 @@
+package org.jembi.bsis.service.report;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.jembi.bsis.constant.CohortConstants;
+import org.jembi.bsis.dto.DonorsAdverseEventsDTO;
+import org.jembi.bsis.model.reporting.Cohort;
+import org.jembi.bsis.model.reporting.Comparator;
+import org.jembi.bsis.model.reporting.DataValue;
+import org.jembi.bsis.model.reporting.Report;
+import org.jembi.bsis.repository.AdverseEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DonorsAdverseEventsReportGenerator {
+  
+  @Autowired
+  private AdverseEventRepository adverseEventRepository;
+  
+  public Report generateDonorsAdverseEventsReport(Long venueId, Date startDate, Date endDate) {
+    Report report = new Report();
+
+    report.setStartDate(startDate);
+    report.setEndDate(endDate);
+    report.setDataValues(getAdverseEventsDataValues(venueId, startDate, endDate));
+
+    return report;
+  }
+
+  private List<DataValue> getAdverseEventsDataValues(Long venueId, Date startDate, Date endDate) {
+    List<DonorsAdverseEventsDTO> dtos = adverseEventRepository.countAdverseEvents(venueId, startDate, endDate);
+
+    List<DataValue> dataValues = new ArrayList<>(dtos.size());
+
+    for (DonorsAdverseEventsDTO dto : dtos) {
+
+      DataValue dataValue = new DataValue();
+      dataValue.setStartDate(startDate);
+      dataValue.setEndDate(endDate);
+      dataValue.setVenue(dto.getVenue());
+      dataValue.setValue(dto.getCount());
+
+      Cohort adverseEventCohort = new Cohort();
+      adverseEventCohort.setCategory(CohortConstants.ADVERSE_EVENT_TYPE_CATEGORY);
+      adverseEventCohort.setComparator(Comparator.EQUALS);
+      adverseEventCohort.setOption(dto.getAdverseEventType().getName());
+      dataValue.addCohort(adverseEventCohort);
+      
+      dataValues.add(dataValue);
+    }
+
+    return dataValues;
+  }
+}
---
+++ src/test/java/org/jembi/bsis/service/report/DonorsAdverseEventsReportGeneratorTests.java
@@ -0,0 +1,147 @@
+package org.jembi.bsis.service.report;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.jembi.bsis.helpers.builders.AdverseEventTypeBuilder.anAdverseEventType;
+import static org.jembi.bsis.helpers.builders.CohortBuilder.aCohort;
+import static org.jembi.bsis.helpers.builders.DataValueBuilder.aDataValue;
+import static org.jembi.bsis.helpers.builders.DonorsAdverseEventsDTOBuilder.aDonorsAdverseEventsDTO;
+import static org.jembi.bsis.helpers.builders.LocationBuilder.aVenue;
+import static org.jembi.bsis.helpers.builders.ReportBuilder.aReport;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.jembi.bsis.constant.CohortConstants;
+import org.jembi.bsis.dto.DonorsAdverseEventsDTO;
+import org.jembi.bsis.model.adverseevent.AdverseEventType;
+import org.jembi.bsis.model.location.Location;
+import org.jembi.bsis.model.reporting.Cohort;
+import org.jembi.bsis.model.reporting.Comparator;
+import org.jembi.bsis.model.reporting.DataValue;
+import org.jembi.bsis.model.reporting.Report;
+import org.jembi.bsis.repository.AdverseEventRepository;
+import org.jembi.bsis.suites.UnitTestSuite;
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class DonorsAdverseEventsReportGeneratorTests extends UnitTestSuite {
+
+  @InjectMocks
+  private DonorsAdverseEventsReportGenerator donorsAdverseEventsReportGenerator;
+
+  @Mock
+  private AdverseEventRepository adverseEventRepository;
+  
+  @Test
+  public void testGenerateAdverseEventsReportForAllVenues_shouldGenerateReportCorrectly() {
+
+    // Set up data
+    Date startDate = new DateTime().minusDays(7).toDate();
+    Date endDate = new DateTime().minusDays(0).toDate();
+
+    Location venueB = aVenue().withName("b venue").build();
+    Location venueA = aVenue().withName("a venue").build();
+
+    AdverseEventType adverseEventTypeSomething = anAdverseEventType().withName("something").build();
+    AdverseEventType adverseEventTypeSomethingReallyBad = anAdverseEventType().withName("something really bad").build();
+    
+    List<DonorsAdverseEventsDTO> adverseEventsDTOs = Arrays.asList(
+        aDonorsAdverseEventsDTO().withAdverseEventType(adverseEventTypeSomething).withVenue(venueA).withCount(4).build(),
+        aDonorsAdverseEventsDTO().withAdverseEventType(adverseEventTypeSomething).withVenue(venueB).withCount(1).build(),
+        aDonorsAdverseEventsDTO().withAdverseEventType(adverseEventTypeSomethingReallyBad).withVenue(venueA).withCount(14).build(),
+        aDonorsAdverseEventsDTO().withAdverseEventType(adverseEventTypeSomethingReallyBad).withVenue(venueB).withCount(3).build()
+    );
+
+    Cohort somethingCohort = aCohort()
+        .withCategory(CohortConstants.ADVERSE_EVENT_TYPE_CATEGORY)
+        .withComparator(Comparator.EQUALS)
+        .withOption("something")
+        .build();
+    Cohort somethingReallyBadCohort = aCohort()
+        .withCategory(CohortConstants.ADVERSE_EVENT_TYPE_CATEGORY)
+        .withComparator(Comparator.EQUALS)
+        .withOption("something really bad")
+        .build();
+
+    List<DataValue> expectedDataValues = Arrays.asList(
+        aDataValue().withStartDate(startDate).withEndDate(endDate).withValue(4L).withVenue(venueA)
+            .withCohort(somethingCohort)
+            .build(),
+        aDataValue().withStartDate(startDate).withEndDate(endDate).withValue(1L).withVenue(venueB)
+            .withCohort(somethingCohort)
+            .build(),
+        aDataValue().withStartDate(startDate).withEndDate(endDate).withValue(14L).withVenue(venueA)
+            .withCohort(somethingReallyBadCohort)
+            .build(),
+        aDataValue().withStartDate(startDate).withEndDate(endDate).withValue(3L).withVenue(venueB)
+            .withCohort(somethingReallyBadCohort)
+            .build()
+    );
+
+    Report expectedReport = aReport().withStartDate(startDate).withEndDate(endDate).withDataValues(expectedDataValues).build();
+
+    // Set up mocks
+    when(adverseEventRepository.countAdverseEvents(null, startDate, endDate)).thenReturn(adverseEventsDTOs);
+
+    // Run test
+    Report returnedReport = donorsAdverseEventsReportGenerator.generateDonorsAdverseEventsReport(null, startDate, endDate);
+
+    // Verify
+    assertThat(returnedReport, is(equalTo(expectedReport)));
+  }
+
+  @Test
+  public void testGenerateAdverseEventsReportForOneVenues_shouldGenerateReportCorrectly() {
+
+    // Set up data
+    Date startDate = new DateTime().minusDays(7).toDate();
+    Date endDate = new DateTime().minusDays(0).toDate();
+
+    Location venueA = aVenue().withId(1L).withName("a venue").build();
+
+    AdverseEventType adverseEventTypeSomething = anAdverseEventType().withName("something").build();
+    AdverseEventType adverseEventTypeSomethingReallyBad = anAdverseEventType().withName("something really bad").build();
+    
+    List<DonorsAdverseEventsDTO> adverseEventsDTOs = Arrays.asList(
+        aDonorsAdverseEventsDTO().withAdverseEventType(adverseEventTypeSomething).withVenue(venueA).withCount(4).build(),
+        aDonorsAdverseEventsDTO().withAdverseEventType(adverseEventTypeSomethingReallyBad).withVenue(venueA).withCount(14).build()
+    );
+
+    Cohort somethingCohort = aCohort()
+        .withCategory(CohortConstants.ADVERSE_EVENT_TYPE_CATEGORY)
+        .withComparator(Comparator.EQUALS)
+        .withOption("something")
+        .build();
+    Cohort somethingReallyBadCohort = aCohort()
+        .withCategory(CohortConstants.ADVERSE_EVENT_TYPE_CATEGORY)
+        .withComparator(Comparator.EQUALS)
+        .withOption("something really bad")
+        .build();
+
+    List<DataValue> expectedDataValues = Arrays.asList(
+        aDataValue().withStartDate(startDate).withEndDate(endDate).withValue(4L).withVenue(venueA)
+            .withCohort(somethingCohort)
+            .build(),
+        aDataValue().withStartDate(startDate).withEndDate(endDate).withValue(14L).withVenue(venueA)
+            .withCohort(somethingReallyBadCohort)
+            .build()
+    );
+
+    Report expectedReport = aReport().withStartDate(startDate).withEndDate(endDate).withDataValues(expectedDataValues).build();
+
+    // Set up mocks
+    when(adverseEventRepository.countAdverseEvents(venueA.getId(), startDate, endDate)).thenReturn(adverseEventsDTOs);
+
+    // Run test
+    Report returnedReport = donorsAdverseEventsReportGenerator.generateDonorsAdverseEventsReport(venueA.getId(), startDate, endDate);
+
+    // Verify
+    assertThat(returnedReport, is(equalTo(expectedReport)));
+  }
+}
