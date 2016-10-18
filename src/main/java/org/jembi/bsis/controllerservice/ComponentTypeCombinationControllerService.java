293f7500c7f99163f706de3af2a1c98b23b3dfdb
mmatiashe <mmatiashe@hitrac.co.zw>
2016-10-18 17:12:28 +0200

    BSIS-1865 Add form endpoint
    Added form endpoint for component type combination.
    The form endpoint returns a backing form, source component types
    and produced component types.

--- src/main/java/org/jembi/bsis/controllerservice/ComponentTypeCombinationControllerService.java
+++ src/main/java/org/jembi/bsis/controllerservice/ComponentTypeCombinationControllerService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.jembi.bsis.factory.ComponentTypeCombinationFactory;
+import org.jembi.bsis.factory.ComponentTypeFactory;
 import org.jembi.bsis.repository.ComponentTypeCombinationRepository;
+import org.jembi.bsis.repository.ComponentTypeRepository;
 import org.jembi.bsis.viewmodel.ComponentTypeCombinationViewModel;
+import org.jembi.bsis.viewmodel.ComponentTypeViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,11 +23,21 @@ public class ComponentTypeCombinationControllerService {
   @Autowired
   private ComponentTypeCombinationFactory componentTypeCombinationFactory;
 
+  @Autowired
+  private ComponentTypeFactory componentTypeFactory;
+
+  @Autowired
+  private ComponentTypeRepository componentTypeRepository;
+
   public List<ComponentTypeCombinationViewModel> getComponentTypeCombinations(boolean includeDeleted) {
     return componentTypeCombinationFactory.createViewModels(componentTypeCombinationRepository.getAllComponentTypeCombinations(includeDeleted));
   }
 
   public ComponentTypeCombinationViewModel findComponentTypeCombinationById(long id){
     return  componentTypeCombinationFactory.createViewModel(componentTypeCombinationRepository.findComponentTypeCombinationById(id));
+  }
+
+  public List<ComponentTypeViewModel> getAllComponentTypes() {
+    return componentTypeFactory.createViewModels(componentTypeRepository.getAllComponentTypesThatCanBeIssued());
   }
 }
