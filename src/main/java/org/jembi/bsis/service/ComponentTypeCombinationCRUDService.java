57cf7d9436f55932718a9a7e10294c01ac5660dd
tumijacob <matome.phoshoko@jembi.org>
2016-10-19 15:18:37 +0200

    BSIS-1868 Remove an extra white space * Removed an extra white space in the ComponentTypeCombinationCRUDService

--- src/main/java/org/jembi/bsis/service/ComponentTypeCombinationCRUDService.java
+++ src/main/java/org/jembi/bsis/service/ComponentTypeCombinationCRUDService.java
@@ -29,7 +29,5 @@ public class ComponentTypeCombinationCRUDService {
     existingCombination.setSourceComponentTypes(componentTypeCombination.getSourceComponentTypes());
 
     return componentTypeCombinationRepository.update(componentTypeCombination);
-
   }
-
 }
