aabead5ad26acffa747bf6d1d82deb2f20f61b6c
tumijacob <matome.phoshoko@jembi.org>
2016-10-19 14:56:31 +0200

    BSIS-1868 Create ComponentTypeCombinationCRUDService with methods create and update * An administrative user needs to be able to add and update componentType combinations
    * Added new CRUD service componentTypeCombination for creating and updating componentTypeCombiontions
    * Added some test cases for creating and updating componentTypeCombinations
    https://jembiprojects.jira.com/browse/BSIS-1868

---
+++ src/main/java/org/jembi/bsis/service/ComponentTypeCombinationCRUDService.java
@@ -0,0 +1,35 @@
+package org.jembi.bsis.service;
+
+import org.jembi.bsis.model.componenttype.ComponentTypeCombination;
+import org.jembi.bsis.repository.ComponentTypeCombinationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class ComponentTypeCombinationCRUDService {
+
+  @Autowired
+  private ComponentTypeCombinationRepository componentTypeCombinationRepository;
+
+  public ComponentTypeCombination createComponentTypeCombination(ComponentTypeCombination componentTypeCombination) {
+    componentTypeCombinationRepository.save(componentTypeCombination);
+    return componentTypeCombination;
+  }
+
+  public ComponentTypeCombination updateComponentTypeCombinations(ComponentTypeCombination componentTypeCombination) {
+
+    ComponentTypeCombination existingCombination = componentTypeCombinationRepository
+        .findComponentTypeCombinationById(componentTypeCombination.getId());
+        
+    existingCombination.setCombinationName(componentTypeCombination.getCombinationName());
+    existingCombination.setComponentTypes(componentTypeCombination.getComponentTypes());
+    existingCombination.setIsDeleted(componentTypeCombination.getIsDeleted());
+    existingCombination.setSourceComponentTypes(componentTypeCombination.getSourceComponentTypes());
+
+    return componentTypeCombinationRepository.update(componentTypeCombination);
+
+  }
+
+}
