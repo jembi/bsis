af2ade203d22052ed15b3dc5ae1d7a97306180f6
rowan84 <rowan.pillay@jembi.org>
2016-10-27 12:15:31 +0200

    BSIS-1927 Create BloodTestController and BloodTestControllerService
    Add BloodTestController and BloodTestControllerService with an endpoint
    to return all Blood Tests.
    
    This commit uses the existing view BloodTestViewModel to return
    the BloodTest data. This is a full view model.

---
+++ src/main/java/org/jembi/bsis/controller/BloodTestController.java
@@ -0,0 +1,30 @@
+package org.jembi.bsis.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jembi.bsis.controllerservice.BloodTestControllerService;
+import org.jembi.bsis.utils.PermissionConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("bloodtests")
+public class BloodTestController {
+
+  @Autowired
+  private BloodTestControllerService bloodTestControllerService;
+
+  @RequestMapping(method = RequestMethod.GET, value = "/search")
+  @PreAuthorize("hasRole('" + PermissionConstants.MANAGE_BLOOD_TESTS + "')")
+  public ResponseEntity<Map<String, Object>> getBloodTests() {
+    Map<String, Object> map = new HashMap<>();
+    map.put("bloodTests", bloodTestControllerService.getAllBloodTests());
+    return new ResponseEntity<>(map, HttpStatus.OK);
+  }
+}
