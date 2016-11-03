5bae07f56637b0b86915562c86af6cc4f8d4bfae
lauravignoli <vignoli.laura@gmail.com>
2016-11-03 13:11:58 +0200

    BSIS-2003 Add missing test to BloodTestResultRepository
    A test for countBloodTestResultsForDonation was missing for BloodTestResultRepository.

--- src/main/java/org/jembi/bsis/repository/BloodTestResultRepository.java
+++ src/main/java/org/jembi/bsis/repository/BloodTestResultRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 import org.jembi.bsis.dto.BloodTestResultDTO;
 import org.jembi.bsis.dto.BloodTestResultExportDTO;
 import org.jembi.bsis.dto.BloodTestTotalDTO;
@@ -20,7 +21,6 @@ public class BloodTestResultRepository extends AbstractRepository<BloodTestResul
   @PersistenceContext
   private EntityManager entityManager;
 
-  // TODO: Test
   public int countBloodTestResultsForDonation(long donationId) {
     return entityManager.createNamedQuery(
         BloodTestResultNamedQueryConstants.NAME_COUNT_BLOOD_TEST_RESULTS_FOR_DONATION, 
