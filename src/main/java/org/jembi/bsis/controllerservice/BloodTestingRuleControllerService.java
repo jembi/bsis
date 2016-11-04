a75e6dcef924f730d86fc010855574318d6dd691
Judge Muzinda <jmuzinda@gmail.com>
2016-11-04 11:12:24 +0200

    BSIS-1973 Add Blood testing rules controller and controller service
    Add Blood testing rules controller and controller service to return all
    blood testing rules.

---
+++ src/main/java/org/jembi/bsis/controllerservice/BloodTestingRuleControllerService.java
@@ -0,0 +1,27 @@
+package org.jembi.bsis.controllerservice;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.jembi.bsis.factory.BloodTestingRuleFactory;
+import org.jembi.bsis.model.bloodtesting.rules.BloodTestingRule;
+import org.jembi.bsis.repository.BloodTestingRuleRepository;
+import org.jembi.bsis.viewmodel.BloodTestingRuleViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class BloodTestingRuleControllerService {
+  
+  @Autowired
+  private BloodTestingRuleFactory bloodTestingRuleFactory;
+  @Autowired
+  private BloodTestingRuleRepository bloodTestingRuleRepository;
+  
+  public List<BloodTestingRuleViewModel> getAllBloodTestingRules(boolean includeDeleted) {
+    List<BloodTestingRule> bloodTestingRules = bloodTestingRuleRepository.getBloodTestingRules(includeDeleted);
+    return bloodTestingRuleFactory.createViewModels(bloodTestingRules);
+  }  
+}
