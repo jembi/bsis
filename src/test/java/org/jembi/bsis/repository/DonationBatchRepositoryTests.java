619e0d4baa7683f3184060ec840f2d57ec57e99f
tumijacob <matome.phoshoko@jembi.org>
2016-11-07 15:12:47 +0200

    BSIS-1923 Add unit tests for the new method findComponentBatchByDonationBatchId * Added tests for the findComponentBatchByDonationBatchId

--- src/test/java/org/jembi/bsis/repository/DonationBatchRepositoryTests.java
+++ src/test/java/org/jembi/bsis/repository/DonationBatchRepositoryTests.java
@@ -2,14 +2,23 @@ package org.jembi.bsis.repository;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.jembi.bsis.helpers.builders.DonationBatchBuilder.aDonationBatch;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
-import org.jembi.bsis.repository.DonationBatchRepository;
+import org.hamcrest.core.IsNull;
+import org.jembi.bsis.helpers.builders.ComponentBatchBuilder;
+import org.jembi.bsis.model.componentbatch.ComponentBatch;
+import org.jembi.bsis.model.donationbatch.DonationBatch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.scalastuff.scalabeans.sig.Mirror.NullValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -48,6 +57,50 @@ public class DonationBatchRepositoryTests {
     int returnedCount = donationBatchRepository.countOpenDonationBatches();
 
     assertThat(returnedCount, is(2));
+  }
+  
+  @Test
+  public void testFindComponentBatchByDonationBatchId_returnsComponentBatch() {
+    ComponentBatch newComponentBatch = ComponentBatchBuilder.aComponentBatch()
+        .build();
+  
+    List<DonationBatch> donationBatches = Arrays.asList(
+        aDonationBatch()
+        .withComponentBatch(newComponentBatch)
+        .buildAndPersist(entityManager),
+        
+        // Excluded by deletion
+        aDonationBatch()
+        .withComponentBatch(newComponentBatch)
+        .thatIsDeleted()
+        .buildAndPersist(entityManager),
+        
+        // Excluded by closure
+        aDonationBatch()
+        .withComponentBatch(newComponentBatch)
+        .thatIsClosed()
+        .buildAndPersist(entityManager) 
+    );  
+    newComponentBatch.setDonationBatch(donationBatches.get(0));
+    newComponentBatch.setDonationBatch(donationBatches.get(1));
+    newComponentBatch.setDonationBatch(donationBatches.get(2));
+    
+    // Test
+    ComponentBatch returnedComponentBatch = donationBatchRepository.findComponentBatchByDonationbatchId(
+        donationBatches.get(0).getId());
+    
+    assertThat(newComponentBatch,is(returnedComponentBatch));
+  }
+  
+  @Test
+  public void testFindComponentBatchByDonationBatchIdWithNoResults_returnsNullComponentBatch() {  
+    DonationBatch donationBatch =  aDonationBatch().buildAndPersist(entityManager);
+       
+    // Test
+    ComponentBatch returnedComponentBatch = donationBatchRepository.findComponentBatchByDonationbatchId(
+        donationBatch.getId());
+    
+    assertThat(returnedComponentBatch,is(nullValue()));
   }
 
 }
