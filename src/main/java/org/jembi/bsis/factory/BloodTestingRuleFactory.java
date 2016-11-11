b288d6ef1e6ac2a90fe53a5e336370d8be3f529b
Judge Muzinda <jmuzinda@gmail.com>
2016-11-11 15:14:41 +0200

    BSIS-1943 Modify Tests to use bloodTest directly
    Remove references to bloodtestcatory and retrieve it directly from bloodtest.

--- src/main/java/org/jembi/bsis/factory/BloodTestingRuleFactory.java
+++ src/main/java/org/jembi/bsis/factory/BloodTestingRuleFactory.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jembi.bsis.backingform.BloodTestingRuleBackingForm;
+import org.jembi.bsis.model.bloodtesting.BloodTest;
 import org.jembi.bsis.model.bloodtesting.rules.BloodTestingRule;
 import org.jembi.bsis.repository.BloodTestRepository;
 import org.jembi.bsis.viewmodel.BloodTestingRuleFullViewModel;
@@ -68,14 +69,15 @@ public class BloodTestingRuleFactory {
   
   public BloodTestingRule createEntity(BloodTestingRuleBackingForm bloodTestingRuleBackingForm) {
     BloodTestingRule bloodTestingRule = new BloodTestingRule();
+    BloodTest bloodTest = bloodTestRepository.findBloodTestById(bloodTestingRuleBackingForm.getBloodTest().getId());
     
     bloodTestingRule.setId(bloodTestingRuleBackingForm.getId());
-    bloodTestingRule.setCategory(bloodTestingRuleBackingForm.getBloodTest().getCategory());
+    bloodTestingRule.setCategory(bloodTest.getCategory());
     bloodTestingRule.setDonationFieldChanged(bloodTestingRuleBackingForm.getDonationFieldChanged());
     bloodTestingRule.setIsDeleted(bloodTestingRuleBackingForm.getIsDeleted());
     bloodTestingRule.setNewInformation(bloodTestingRuleBackingForm.getNewInformation());
     bloodTestingRule.setPattern(bloodTestingRuleBackingForm.getPattern());
-    bloodTestingRule.setBloodTest(bloodTestRepository.findBloodTestById(bloodTestingRuleBackingForm.getBloodTest().getId()));
+    bloodTestingRule.setBloodTest(bloodTest);
     bloodTestingRule.setPendingTestsIds(StringUtils.join(bloodTestingRuleBackingForm.getPendingTestsIds(), ','));
     return bloodTestingRule;
   }
