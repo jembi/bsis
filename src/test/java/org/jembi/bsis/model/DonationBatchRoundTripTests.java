e68a62a5ca877eba046fec6928adbbd823d87095
Trevor Gowing <trev.gowing@gmail.com>
2016-11-22 09:51:42 +0200

    BSIS-2075 Code review feedback completed

--- src/test/java/org/jembi/bsis/model/DonationBatchRoundTripTests.java
+++ src/test/java/org/jembi/bsis/model/DonationBatchRoundTripTests.java
@@ -10,20 +10,23 @@ import javax.validation.ConstraintViolationException;
 import static org.jembi.bsis.helpers.builders.DonationBatchBuilder.aDonationBatch;
 import static org.jembi.bsis.helpers.builders.LocationBuilder.aLocation;
 
-public class DonationBatchRoundTripTest extends ContextDependentTestSuite {
+public class DonationBatchRoundTripTests extends ContextDependentTestSuite {
+
   @Test
   public void testPersistValidDonationBatch() {
-    aDonationBatch().withVenue(aLocation().buildAndPersist(entityManager)).withDonationBatchDate(new Date()).buildAndPersist(entityManager);
+    aDonationBatch()
+      .withVenue(aLocation().build())
+      .withDonationBatchDate(new Date())
+      .buildAndPersist(entityManager);
   }
 
-
   @Test (expected = ConstraintViolationException.class)
-  public void testPersistDonationBatchDateWithNull_ShouldThrow()  {
+  public void testPersistDonationBatchDateWithNoDonationBatchDate_ShouldThrow()  {
     aDonationBatch().withDonationBatchDate(null).buildAndPersist(entityManager);
   }
 
   @Test (expected = ConstraintViolationException.class)
-  public void testPersistDonationBatchLocationWithNull_ShouldThrow()  {
+  public void testPersistDonationBatchWithNoLocation_ShouldThrow()  {
     aDonationBatch().withVenue(null).buildAndPersist(entityManager);
   }
 }
