dfef437af0d0a06c9a4a53132f26fe5aaeed822a
Tresor <tresor.mvumbi@jembi.org>
2017-01-13 14:42:49 +0200

    BSIS-2348 Check that component weight <= parent's weight
    To prevent recording a child component with a weight greater than the parent's weight

--- src/test/java/org/jembi/bsis/backingform/validator/ComponentBackingFormValidatorTests.java
+++ src/test/java/org/jembi/bsis/backingform/validator/ComponentBackingFormValidatorTests.java
@@ -1,14 +1,19 @@
 package org.jembi.bsis.backingform.validator;
 
 import static org.jembi.bsis.helpers.builders.ComponentBackingFormBuilder.aComponentBackingForm;
+import static org.jembi.bsis.helpers.builders.ComponentBuilder.aComponent;
+import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 
 import org.jembi.bsis.backingform.ComponentBackingForm;
+import org.jembi.bsis.model.component.Component;
+import org.jembi.bsis.repository.ComponentRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.validation.Errors;
 import org.springframework.validation.MapBindingResult;
@@ -19,13 +24,21 @@ public class ComponentBackingFormValidatorTests {
   @InjectMocks
   private ComponentBackingFormValidator validator;
 
+  @Mock
+  private ComponentRepository componentRepository;
+
   @Test
   public void testValidateComponentWithWeight_hasNoErrors() throws Exception {
     // set up data
     ComponentBackingForm form = aComponentBackingForm()
+        .withId(1L)
         .withWeight(333)
         .build();
+    Component component = aComponent().build();
+
     Errors errors = new MapBindingResult(new HashMap<String, String>(), "Component");
+
+    when(componentRepository.findComponent(1L)).thenReturn(component);
 
     // run test
     validator.validate(form, errors);
@@ -38,9 +51,13 @@ public class ComponentBackingFormValidatorTests {
   public void testValidateComponentWithNoWeight_hasNoErrors() throws Exception {
     // set up data
     ComponentBackingForm form = aComponentBackingForm()
+        .withId(1L)
         .withWeight(null)
         .build();
+    Component component = aComponent().build();
     Errors errors = new MapBindingResult(new HashMap<String, String>(), "Component");
+
+    when(componentRepository.findComponent(1L)).thenReturn(component);
 
     // run test
     validator.validate(form, errors);
@@ -53,9 +70,13 @@ public class ComponentBackingFormValidatorTests {
   public void testValidateComponentWithNegativeWeight_hasInvalidWeightError() throws Exception {
     // set up data
     ComponentBackingForm form = aComponentBackingForm()
+        .withId(1L)
         .withWeight(-4)
         .build();
+    Component component = aComponent().build();
     Errors errors = new MapBindingResult(new HashMap<String, String>(), "Component");
+
+    when(componentRepository.findComponent(1L)).thenReturn(component);
 
     // run test
     validator.validate(form, errors);
@@ -70,9 +91,13 @@ public class ComponentBackingFormValidatorTests {
   public void testValidateComponentWithTooLargeWeight_hasInvalidWeightError() throws Exception {
     // set up data
     ComponentBackingForm form = aComponentBackingForm()
+        .withId(1L)
         .withWeight(1000)
         .build();
     Errors errors = new MapBindingResult(new HashMap<String, String>(), "Component");
+    Component component = aComponent().build();
+
+    when(componentRepository.findComponent(1L)).thenReturn(component);
 
     // run test
     validator.validate(form, errors);
@@ -83,4 +108,25 @@ public class ComponentBackingFormValidatorTests {
         errors.getFieldErrors().get(0).getDefaultMessage());
   }
 
+  @Test
+  public void testValidateComponentWithWeightGreaterThanParentWeight_hasInvalidWeightError() {
+    // set up data
+    ComponentBackingForm form = aComponentBackingForm()
+        .withId(1L)
+        .withWeight(500)
+        .build();
+    Errors errors = new MapBindingResult(new HashMap<String, String>(), "Component");
+    Component parentComponent = aComponent().withId(1L).withWeight(450).build();
+    Component component = aComponent().withParentComponent(parentComponent).build();
+
+    when(componentRepository.findComponent(1L)).thenReturn(component);
+
+    // run test
+    validator.validate(form, errors);
+
+    // do checks
+    Assert.assertEquals("One error", 1, errors.getErrorCount());
+    Assert.assertEquals("Invalid weight error", "weight should not be greater than parent component weight",
+        errors.getFieldErrors().get(0).getDefaultMessage());    
+  }
 }
