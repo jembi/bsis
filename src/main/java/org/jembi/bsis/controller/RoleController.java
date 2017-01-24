25ef24c7e4b639be5a08c976f4abed475b478fc8
Dagmar Timler <dagmar.timler@gmail.com>
2017-01-24 15:05:36 +0200

    BSIS-2440: Cleanup RoleController
    Cleanup of RoleController so that the Eclipse warnings are removed and the
    controller is more in line with BSIS design guidelines.

--- src/main/java/org/jembi/bsis/controller/RoleController.java
+++ src/main/java/org/jembi/bsis/controller/RoleController.java
@@ -1,7 +1,6 @@
 package org.jembi.bsis.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,13 +9,12 @@ import javax.validation.Valid;
 import org.jembi.bsis.backingform.RoleBackingForm;
 import org.jembi.bsis.backingform.validator.RoleBackingFormValidator;
 import org.jembi.bsis.factory.RoleFactory;
-import org.jembi.bsis.model.user.Permission;
 import org.jembi.bsis.model.user.Role;
 import org.jembi.bsis.repository.RoleRepository;
 import org.jembi.bsis.utils.PermissionConstants;
+import org.jembi.bsis.viewmodel.RoleViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -24,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,9 +37,6 @@ public class RoleController {
 
   @Autowired
   private RoleFactory roleFactory;
-
-  public RoleController() {
-  }
 
   @InitBinder
   protected void initBinder(WebDataBinder binder) {
@@ -59,57 +55,43 @@ public class RoleController {
   @PreAuthorize("hasRole('" + PermissionConstants.MANAGE_ROLES + "')")
   public Map<String, Object> editRoleFormGenerator() {
     Map<String, Object> map = new HashMap<String, Object>();
-    addAllPermissionsToModel(map);
+    map.put("permissions", roleRepository.getAllPermissionsByName());
     return map;
   }
 
   @RequestMapping(value = "{id}", method = RequestMethod.PUT)
   @PreAuthorize("hasRole('" + PermissionConstants.MANAGE_ROLES + "')")
-  public ResponseEntity updateRole(
-      @Valid @RequestBody RoleBackingForm form, @PathVariable Long id) {
-
-    Role updatedRole = null;
+  public RoleViewModel updateRole(@Valid @RequestBody RoleBackingForm form, @PathVariable Long id) {
     form.setId(id);
-    updatedRole = roleRepository.updateRole(form.getRole());
-    return new ResponseEntity(roleFactory.createViewModel(updatedRole), HttpStatus.OK);
+    Role updatedRole = roleRepository.updateRole(form.getRole());
+    return roleFactory.createViewModel(updatedRole);
 
   }
 
   @RequestMapping(value = "{id}", method = RequestMethod.GET)
   @PreAuthorize("hasRole('" + PermissionConstants.MANAGE_ROLES + "')")
-  public ResponseEntity getRoleBydId(@PathVariable Long id) {
+  public Map<String, Object> getRoleBydId(@PathVariable Long id) {
     Map<String, Object> map = new HashMap<String, Object>();
     Role role = roleRepository.findRoleDetailById(id);
     map.put("role", roleFactory.createViewModel(role));
-    return new ResponseEntity(map, HttpStatus.OK);
-
+    return map;
   }
 
   @RequestMapping(method = RequestMethod.POST)
   @PreAuthorize("hasRole('" + PermissionConstants.MANAGE_ROLES + "')")
-  public ResponseEntity addRole(
-      @Valid @RequestBody RoleBackingForm form) {
-
+  @ResponseStatus(HttpStatus.CREATED)
+  public RoleViewModel addRole(@Valid @RequestBody RoleBackingForm form) {
     Role role = new Role();
     role.setName(form.getName());
     role.setDescription(form.getDescription());
     role.setPermissions(form.getPermissions());
     role = roleRepository.addRole(role);
-    return new ResponseEntity(roleFactory.createViewModel(role), HttpStatus.CREATED);
+    return roleFactory.createViewModel(role);
   }
 
   @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
   @PreAuthorize("hasRole('" + PermissionConstants.MANAGE_ROLES + "')")
-  public ResponseEntity deleteRoleBydId(@PathVariable Long id) {
-
+  public void deleteRoleBydId(@PathVariable Long id) {
     roleRepository.deleteRole(id);
-    return new ResponseEntity<Role>(HttpStatus.NO_CONTENT);
-
   }
-
-  private void addAllPermissionsToModel(Map<String, Object> map) {
-    List<Permission> permissions = roleRepository.getAllPermissionsByName();
-    map.put("permissions", permissions);
-  }
-
 }
