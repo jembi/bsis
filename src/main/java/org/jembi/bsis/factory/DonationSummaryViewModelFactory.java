9112e3750a38fb995ad270545cbd75b956565aa6
lauravignoli <vignoli.laura@gmail.com>
2017-02-14 12:58:20 +0200

    BSIS-2597 Use DonationViewModel instead of DonationSummaryViewModel for testBatches
    When returning all donations for a test batch, the DonationSummaryViewModel was being used, but it was just returning a DonationViewModel.
    
    Removed the creation of those view models from the donation summary factory to the test batch factory, as getting the donations for a test batch was only used for the test batches section.

--- src/main/java/org/jembi/bsis/factory/DonationSummaryViewModelFactory.java
+++ src/main/java/org/jembi/bsis/factory/DonationSummaryViewModelFactory.java
@@ -3,10 +3,7 @@ package org.jembi.bsis.factory;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jembi.bsis.model.donation.BloodTypingMatchStatus;
 import org.jembi.bsis.model.donation.Donation;
-import org.jembi.bsis.model.donationbatch.DonationBatch;
-import org.jembi.bsis.model.testbatch.TestBatch;
 import org.jembi.bsis.viewmodel.DonationSummaryViewModel;
 import org.springframework.stereotype.Service;
 
@@ -15,29 +12,6 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class DonationSummaryViewModelFactory {
-
-
-  /**
-   * Creates a list of DonationSummaryViewModel objects from a test batch, with the option of
-   * filtering by blood typing match status if the bloodTypingMatchStatus parameter is not null.
-   *
-   * @param testBatch the test batch
-   * @param bloodTypingMatchStatus the blood typing match status
-   * @return the list< donation summary view model>
-   */
-  public List<DonationSummaryViewModel> createDonationSummaryViewModels(
-      TestBatch testBatch, BloodTypingMatchStatus bloodTypingMatchStatus) {
-    List<DonationSummaryViewModel> donationSummaryViewModels = new ArrayList<>();
-    for (DonationBatch donationBatch : testBatch.getDonationBatches()) {
-      for (Donation donation : donationBatch.getDonations()) {
-        if (bloodTypingMatchStatus == null || donation.getBloodTypingMatchStatus().equals(bloodTypingMatchStatus)) {
-          donationSummaryViewModels.add(new DonationSummaryViewModel(donation));
-        }
-      }
-    }
-    return donationSummaryViewModels;
-  }
-
 
   /**
    * Creates a list of DonationSummaryViewModel objects from a list of donations. The models are a
