8aa0a89713ed089f3b972dab2465d8296848070b
Tresor <tresor.mvumbi@jembi.org>
2017-02-14 07:45:50 +0200

    BSIS-2543 Move /donations/summaries to /postdonationcounsellings/search
    /donations/summaries retrieves the list of donations flagged for counselling from postDonationCounsellingRepository; it therefore makes more sense to move it to PostDonationCounsellingController.

--- src/main/java/org/jembi/bsis/controller/PostDonationCounsellingController.java
+++ src/main/java/org/jembi/bsis/controller/PostDonationCounsellingController.java
@@ -1,7 +1,10 @@
 package org.jembi.bsis.controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -11,11 +14,18 @@ import org.jembi.bsis.backingform.PostDonationCounsellingBackingForm;
 import org.jembi.bsis.backingform.validator.PostDonationCounsellingBackingFormValidator;
 import org.jembi.bsis.controllerservice.LocationControllerService;
 import org.jembi.bsis.controllerservice.PostDonationCounsellingControllerService;
+import org.jembi.bsis.factory.DonationSummaryViewModelFactory;
 import org.jembi.bsis.model.counselling.CounsellingStatus;
+import org.jembi.bsis.model.donation.Donation;
+import org.jembi.bsis.repository.PostDonationCounsellingRepository;
 import org.jembi.bsis.utils.PermissionConstants;
+import org.jembi.bsis.utils.PermissionUtils;
 import org.jembi.bsis.viewmodel.CounsellingStatusViewModel;
+import org.jembi.bsis.viewmodel.DonationSummaryViewModel;
 import org.jembi.bsis.viewmodel.PostDonationCounsellingViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -23,6 +33,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,6 +44,10 @@ public class PostDonationCounsellingController {
   private PostDonationCounsellingControllerService postDonationCounsellingControllerService;
   @Autowired
   private PostDonationCounsellingBackingFormValidator postDonationCounsellingBackingFormValidator;
+  @Autowired
+  private PostDonationCounsellingRepository postDonationCounsellingRepository;
+  @Autowired
+  private DonationSummaryViewModelFactory donationSummaryViewModelFactory;
   
   @InitBinder
   public void initBinder(WebDataBinder binder) {
@@ -68,4 +83,26 @@ public class PostDonationCounsellingController {
     return map;
   }
 
+  @RequestMapping(value = "/search", method = RequestMethod.GET)
+  @PreAuthorize("hasRole('" + PermissionConstants.VIEW_DONOR + "')")
+  public List<DonationSummaryViewModel> getDonationSummaries(
+      @RequestParam(value = "flaggedForCounselling", required = true) boolean flaggedForCounselling,
+      @RequestParam(value = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date startDate,
+      @RequestParam(value = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date endDate,
+      @RequestParam(value = "venue", required = false) List<Long> venues) {
+
+    if (flaggedForCounselling) {
+
+      if (!PermissionUtils.loggedOnUserHasPermission(PermissionConstants.VIEW_POST_DONATION_COUNSELLING_DONORS)) {
+        throw new AccessDeniedException("You do not have permission to view post donation counselling donors.");
+      }
+
+      List<Donation> donations = postDonationCounsellingRepository.findDonationsFlaggedForCounselling(
+          startDate, endDate, venues == null ? null : new HashSet<>(venues));
+      return donationSummaryViewModelFactory.createFullDonationSummaryViewModels(donations);
+    }
+
+    // Just return an empty list for now. This could return the full list of donations if needed.
+    return Collections.emptyList();
+  }
 }
