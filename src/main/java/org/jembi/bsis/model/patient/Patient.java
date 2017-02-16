b9fa41b3295fb2faae923c1a379f389de3fcdb3f
Tresor <tresor.mvumbi@jembi.org>
2017-02-16 13:26:35 +0200

    BSIS-2610 Add Patient entity
    Add Patient entity

---
+++ src/main/java/org/jembi/bsis/model/patient/Patient.java
@@ -0,0 +1,117 @@
+package org.jembi.bsis.model.patient;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.jembi.bsis.model.BaseEntity;
+import org.jembi.bsis.model.util.Gender;
+
+@Entity
+public class Patient extends BaseEntity{
+
+  @NotBlank
+  @Column(length = 20)
+  private String name1;
+
+  @NotBlank
+  @Column(length = 20)
+  private String name2;
+
+  private Date dateOfBirth;
+
+  @Enumerated(EnumType.STRING)
+  private Gender gender;
+
+  @Column(length = 20)
+  private String patientNumber;
+
+  @Column(length = 20)
+  private String hospitalBloodBankNumber;
+
+  @Column(length = 20)
+  private String hospitalWardNumber;
+
+  @Column(length = 50)
+  private String bloodAbo;
+
+  @Column(length = 50)
+  private String bloodRh;
+
+  public String getName1() {
+    return name1;
+  }
+
+  public void setName1(String name1) {
+    this.name1 = name1;
+  }
+
+  public String getName2() {
+    return name2;
+  }
+
+  public void setName2(String name2) {
+    this.name2 = name2;
+  }
+
+  public Date getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(Date dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public Gender getGender() {
+    return gender;
+  }
+
+  public void setGender(Gender gender) {
+    this.gender = gender;
+  }
+
+  public String getPatientNumber() {
+    return patientNumber;
+  }
+
+  public void setPatientNumber(String patientNumber) {
+    this.patientNumber = patientNumber;
+  }
+
+  public String getHospitalBloodBankNumber() {
+    return hospitalBloodBankNumber;
+  }
+
+  public void setHospitalBloodBankNumber(String hospitalBloodBankNumber) {
+    this.hospitalBloodBankNumber = hospitalBloodBankNumber;
+  }
+
+  public String getHospitalWardNumber() {
+    return hospitalWardNumber;
+  }
+
+  public void setHospitalWardNumber(String hospitalWardNumber) {
+    this.hospitalWardNumber = hospitalWardNumber;
+  }
+
+  public String getBloodAbo() {
+    return bloodAbo;
+  }
+
+  public void setBloodAbo(String bloodAbo) {
+    this.bloodAbo = bloodAbo;
+  }
+
+  public String getBloodRh() {
+    return bloodRh;
+  }
+
+  public void setBloodRh(String bloodRh) {
+    this.bloodRh = bloodRh;
+  }
+
+}
