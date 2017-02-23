e2e3f82210c8396e63d0f8cacd879df7c1a70123
Dagmar Timler <dagmar.timler@gmail.com>
2017-02-23 09:35:26 +0200

    BSIS-2628: Add POST endpoint for T.ReactionType
    Added POST endpoint for TransfusionReactionTypeController so that we
    are able to create a new TransfusionReactionType.

--- src/main/java/org/jembi/bsis/controller/TransfusionReactionTypeController.java
+++ src/main/java/org/jembi/bsis/controller/TransfusionReactionTypeController.java
@@ -3,6 +3,7 @@ package org.jembi.bsis.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jembi.bsis.backingform.TransfusionReactionTypeBackingForm;
 import org.jembi.bsis.controllerservice.TransfusionReactionTypeControllerService;
 import org.jembi.bsis.utils.PermissionConstants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,6 +28,15 @@ public class TransfusionReactionTypeController {
     Map<String, Object> map = new HashMap<>();
     map.put("transfusionReactionTypes", transfusionReactionTypeControllerService.getTransfusionReactionTypes());
     return new ResponseEntity<>(map, HttpStatus.OK);
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  @PreAuthorize("hasAnyRole('" + PermissionConstants.MANAGE_TRANSFUSION_REACTION_TYPES + "')")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Map<String, Object> addTransfusionReactionTypes(TransfusionReactionTypeBackingForm backingForm) {
+    Map<String, Object> map = new HashMap<>();
+    map.put("transfusionReactionType", transfusionReactionTypeControllerService.createTransfusionReactionType(backingForm));
+    return map;
   }
 
 }
