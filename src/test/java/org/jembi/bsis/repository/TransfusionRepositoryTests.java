e178f0e58b719ad482dc91239aa2e759694597de
Judge Muzinda <jmuzinda@gmail.com>
2017-03-03 17:15:28 +0200

    BSIS-2750 Check that tests are returning expected DTOS and add brackets to query
    Check that tests are returning expected dtos in the correct state, to ensure that
    the query is executing as expected and add brackets to the query to ensure that the
    seperate query segments execute as units and do not encroach or modify other query conditions.

--- src/test/java/org/jembi/bsis/repository/TransfusionRepositoryTests.java
+++ src/test/java/org/jembi/bsis/repository/TransfusionRepositoryTests.java
@@ -3,12 +3,15 @@ package org.jembi.bsis.repository;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.jembi.bsis.helpers.builders.ComponentBuilder.aComponent;
+import static org.jembi.bsis.helpers.builders.TransfusionSummaryDTOBuilder.aTransfusionSummaryDTO;
+import static org.jembi.bsis.helpers.matchers.TransfusionSummaryDTOMatcher.hasSameStateAsTransfusionSummaryDTO;
 import static org.jembi.bsis.helpers.builders.LocationBuilder.aUsageSite;
 import static org.jembi.bsis.helpers.builders.PatientBuilder.aPatient;
 import static org.jembi.bsis.helpers.builders.TransfusionReactionTypeBuilder.aTransfusionReactionType;
 import static org.jembi.bsis.helpers.builders.TransfusionBuilder.aTransfusion;
 import static org.jembi.bsis.helpers.matchers.TransfusionMatcher.hasSameStateAsTransfusion;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -16,6 +19,7 @@ import org.hamcrest.core.IsNull;
 import org.jembi.bsis.dto.TransfusionSummaryDTO;
 import org.jembi.bsis.model.location.Location;
 import org.jembi.bsis.model.transfusion.Transfusion;
+import org.jembi.bsis.model.transfusion.TransfusionOutcome;
 import org.jembi.bsis.model.transfusion.TransfusionReactionType;
 import org.jembi.bsis.suites.SecurityContextDependentTestSuite;
 import org.joda.time.DateTime;
@@ -74,7 +78,7 @@ public class TransfusionRepositoryTests extends SecurityContextDependentTestSuit
             .withName1("Name 1")
             .withName2("Name 1")
             .build())
-        .withTransfusionReactionType(transfusionReactionType)
+        .withTransfusionReactionType(null)
         .withReceivedFrom(aUsageSite()
             .withName("Received From")
             .buildAndPersist(entityManager))
@@ -151,5 +155,81 @@ public class TransfusionRepositoryTests extends SecurityContextDependentTestSuit
     
     // check that the transfusion summary count returned is equal to persisted transfusions count for the usageSite
     assertThat(transfusionSummaryDTOs.size(), is(1));
+  }
+  
+  @Test
+  public void testFindTransfusionSummaryRecordedForUsageSiteForPeriod_shouldReturnDTOSWithInPeriodRange() {
+    
+    Date startDate = new DateTime().minusDays(60).toDate();
+    Date endDate = new DateTime().plusDays(4).toDate();
+    Location receivedFrom = aUsageSite()
+        .withName("Harare")
+        .buildAndPersist(entityManager);
+    
+    Transfusion expectedTransfusion1 = aTransfusion()
+        .withDonationIdentificationNumber("1234567")
+        .withDateTransfused(new Date())
+        .withPatient(aPatient()
+            .withName1("Name 1")
+            .withName2("Name 1")
+            .build())
+        .withReceivedFrom(receivedFrom)
+        .withComponent(aComponent()
+            .buildAndPersist(entityManager))
+        .withTransfusionOutcome(TransfusionOutcome.TRANSFUSION_REACTION_OCCURRED)
+        .withTransfusionReactionType(aTransfusionReactionType().withName("Reaction Name1").buildAndPersist(entityManager))
+        .buildAndPersist(entityManager);
+    
+    Transfusion expectedTransfusion2 = aTransfusion()
+      .withDonationIdentificationNumber("1234581")
+      .withDateTransfused(new Date())
+      .withPatient(aPatient()
+          .withName1("Name 2")
+          .withName2("Name 2")
+          .build())
+      .withReceivedFrom(receivedFrom)
+      .withTransfusionOutcome(TransfusionOutcome.TRANSFUSED_UNEVENTFULLY)
+      .withTransfusionReactionType(null)
+      .withComponent(aComponent()
+          .buildAndPersist(entityManager))
+      .buildAndPersist(entityManager);
+    
+    // exclude by transfusion date
+    aTransfusion()
+      .withDonationIdentificationNumber("1234576")
+      .withDateTransfused(new DateTime().plusDays(1000).toDate())
+      .withPatient(aPatient()
+          .withName1("Name 3")
+          .withName2("Name 3")
+          .build())
+      .withReceivedFrom(receivedFrom)
+      .withTransfusionOutcome(TransfusionOutcome.TRANSFUSION_REACTION_OCCURRED)
+      .withTransfusionReactionType(aTransfusionReactionType().withName("Reaction Name2").buildAndPersist(entityManager))
+      .withComponent(aComponent()
+          .buildAndPersist(entityManager))
+      .buildAndPersist(entityManager);
+    
+    List<TransfusionSummaryDTO> expectedTransfusionSummaryDTOs = new ArrayList<>();
+
+    expectedTransfusionSummaryDTOs.add(aTransfusionSummaryDTO()
+        .withCount(1)
+        .withTransfusionOutcome(expectedTransfusion2.getTransfusionOutcome())
+        .withTransfusionReactionType(expectedTransfusion2.getTransfusionReactionType())
+        .withTransfusionSite(expectedTransfusion2.getReceivedFrom())
+        .build());  
+    expectedTransfusionSummaryDTOs.add(aTransfusionSummaryDTO()
+        .withCount(1)
+        .withTransfusionOutcome(expectedTransfusion1.getTransfusionOutcome())
+        .withTransfusionReactionType(expectedTransfusion1.getTransfusionReactionType())
+        .withTransfusionSite(expectedTransfusion1.getReceivedFrom())
+        .build());  
+    
+    List<TransfusionSummaryDTO> returnedTransfusionSummaryDTOs = transfusionRepository.findTransfusionSummaryRecordedForUsageSiteForPeriod(receivedFrom.getId(), startDate, endDate);
+    
+    // check that the transfusion summary count returned is for transfusions within range
+    assertThat(returnedTransfusionSummaryDTOs.size(), is(2));
+    // check that returned DTOs are same as expected
+    assertThat(returnedTransfusionSummaryDTOs.get(0), hasSameStateAsTransfusionSummaryDTO(expectedTransfusionSummaryDTOs.get(0)));
+    assertThat(returnedTransfusionSummaryDTOs.get(1), hasSameStateAsTransfusionSummaryDTO(expectedTransfusionSummaryDTOs.get(1)));
   }
 }
