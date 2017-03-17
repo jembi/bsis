e57d8faa59e48274941b49b432b564b81e904ae5
lauravignoli <vignoli.laura@gmail.com>
2017-03-17 13:45:44 +0200

    REVERT BSIS-2902 Update UuidRepository generator logic
    Use random uuid generation only for HSQL dialect, instead of using it for when an exception is thrown. (reverted from commit e5180e08a74f0ed1898e2c4850b53d0efa7ccff2)

--- src/main/java/org/jembi/bsis/repository/UuidRepository.java
+++ src/main/java/org/jembi/bsis/repository/UuidRepository.java
@@ -5,9 +5,6 @@ import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.HSQLDialect;
-import org.hibernate.engine.spi.SessionImplementor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,15 +16,13 @@ public class UuidRepository {
   private EntityManager em;
 
   public UUID generateBinaryUUID() {
-    SessionImplementor sessionImplementor = (SessionImplementor) em.getDelegate();
-    Dialect dialect = sessionImplementor.getFactory().getDialect();
-    // HSQL does not support the sql function. The below is a work-around so that
-    // the JUnit suite, which uses HSQL, will work.
-    if (dialect instanceof HSQLDialect) {
-      return UUID.randomUUID();
-    } else {
-      return (UUID) em.createQuery("SELECT GENERATEBINARYUUID()").getSingleResult();
+    UUID uuid = null;
+    try {
+      uuid = (UUID) em.createQuery("SELECT GENERATEBINARYUUID()").getSingleResult();
+    } catch (Exception e) {
+      uuid = UUID.randomUUID();
     }
+    return uuid;
   }
 
 }
