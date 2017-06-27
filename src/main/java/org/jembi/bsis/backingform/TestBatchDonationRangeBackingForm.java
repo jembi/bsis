686909d7fda25d9512e2c77e4f3302dcbc2787a1
Patience Mpofu <patience.mpofu@jembi.org>
2017-06-27 17:59:26 +0200

    BSIS-3577 Add TestBatchDonationRangeBackingForm
    This change creates a donation range backing form for addint test
    samples to a testbatch.

---
+++ src/main/java/org/jembi/bsis/backingform/TestBatchDonationRangeBackingForm.java
@@ -0,0 +1,32 @@
+package org.jembi.bsis.backingform;
+
+public class TestBatchDonationRangeBackingForm {
+
+  private String testBatchId;
+  private String fromDIN;
+  private String toDIN;
+
+  public String getTestBatchId() {
+    return testBatchId;
+  }
+
+  public void setTestBatchId(String testBatchId) {
+    this.testBatchId = testBatchId;
+  }
+
+  public String getFromDIN() {
+    return fromDIN;
+  }
+
+  public void setFromDIN(String fromDIN) {
+    this.fromDIN = fromDIN;
+  }
+
+  public String getToDIN() {
+    return toDIN;
+  }
+
+  public void setToDIN(String toDIN) {
+    this.toDIN = toDIN;
+  }
+}
