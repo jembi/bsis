216889e4b6268f47d69f026efbeb98b39611837b
Trevor Gowing <trev.gowing@gmail.com>
2018-06-27 15:46:23 +0200

    BSIS-3693 Replace multiple @JsonIgnore with @JsonIgnoreProperties
    Cleaner

--- src/main/java/org/jembi/bsis/backingform/TestBatchBackingForm.java
+++ src/main/java/org/jembi/bsis/backingform/TestBatchBackingForm.java
@@ -1,22 +1,22 @@
 package org.jembi.bsis.backingform;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import org.jembi.bsis.model.testbatch.TestBatchStatus;
+
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.jembi.bsis.model.testbatch.TestBatchStatus;
-import org.jembi.bsis.viewmodel.DonationFullViewModel;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TestBatchBackingForm {
 
   private UUID id;
@@ -24,44 +24,4 @@ public class TestBatchBackingForm {
   private Date testBatchDate;
   private LocationBackingForm location;
   private boolean backEntry;
-
-  @JsonIgnore
-  public void setDonations(List<DonationFullViewModel> donations) {
-    // Ignore
-  }
-
-  @JsonIgnore
-  public void setReadyForReleaseCount(int count) {
-    // Ignore value
-  }
-  
-  @JsonIgnore
-  public void setBatchNumber(String batchNumber) {
-    // Ignore value
-  }
-  
-  @JsonIgnore
-  public void setNotes(String notes) {
-    // Ignore
-  }
-  
-  @JsonIgnore
-  public void setNumSamples(int numSamples) {
-    //Ignore
-  }
-  
-  @JsonIgnore
-  public void setNumReleasedSamples(int numReleasedSamples) {
-    //Ignore
-  }
-  
-  @JsonIgnore
-  public void setPermissions(Map<String, Boolean> permissions) {
-    // Ignore
-  }
-
-  @JsonIgnore
-  public void setLastUpdated(Date lastUpdated) {
-    // Ignore
-  }
 }
