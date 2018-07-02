7e6ce2f4d8f6a4e36a07b1560004996c993d277b
Trevor Gowing <trev.gowing@gmail.com>
2018-07-02 16:16:54 +0200

    BSIS-3591 Validate backing form only
    I think validating the database state in the backing form validators is
    mixing concerns. Want to avoid two sets of point queries for donations.
    
    Database state validation should be done in entity validators or
    CRUDService.

--- src/main/java/org/jembi/bsis/backingform/validator/TestBatchDonationsBackingFormValidator.java
+++ src/main/java/org/jembi/bsis/backingform/validator/TestBatchDonationsBackingFormValidator.java
@@ -1,55 +1,20 @@
 package org.jembi.bsis.backingform.validator;
 
-import java.util.UUID;
-
 import org.jembi.bsis.backingform.TestBatchDonationsBackingForm;
-import org.jembi.bsis.model.donation.Donation;
-import org.jembi.bsis.model.testbatch.TestBatch;
-import org.jembi.bsis.model.testbatch.TestBatchStatus;
-import org.jembi.bsis.repository.DonationRepository;
-import org.jembi.bsis.repository.TestBatchRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
 @Component
 public class TestBatchDonationsBackingFormValidator extends BaseValidator<TestBatchDonationsBackingForm> {
 
-  @Autowired
-  private DonationRepository donationRepository;
-
-  @Autowired
-  private TestBatchRepository testBatchRepository;
-
   @Override
   public void validateForm(TestBatchDonationsBackingForm form, Errors errors) {
-
-    TestBatch testBatch = null;
     if (form.getTestBatchId() == null) {
       errors.rejectValue("testBatchId", "errors.required", "testBatchId is required");
-    } else {
-      testBatch = testBatchRepository.findTestBatchById(form.getTestBatchId());
-      if (testBatch == null) {
-        errors.rejectValue("testBatchId", "errors.invalid", "invalid testBatchId");
-      } else if (testBatch.getStatus().equals(TestBatchStatus.RELEASED)) {
-        errors.rejectValue("testBatchId", "errors.testBatchIsReleased", "testBatch has been released");
-      }
     }
 
-    if (form.getDonationIds() != null) {
-      for (UUID id : form.getDonationIds()) {
-        validateDonation(donationRepository.findDonationById(id), testBatch, errors,
-            form.getDonationIds().indexOf(id));
-      }
-    }
-  }
-
-  private void validateDonation(Donation donation, TestBatch testBatch, Errors errors, int index) {
-    if (donation == null) {
-      errors.rejectValue("donationIds[" + index + "]", "errors.invalid", "invalid donationId");
-    } else if (testBatch != null && !donation.getTestBatch().equals(testBatch)) {
-      errors.rejectValue("donationIds[" + index + "]", "errors.donationBelongsToAnotherTestBatch",
-          "This donation belongs to another test batch");
+    if (form.getDonationIds() == null || form.getDonationIds().isEmpty()) {
+      errors.rejectValue("donationIds", "errors.required", "donationIds may not be null or empty");
     }
   }
 
--- src/test/java/org/jembi/bsis/validator/TestBatchDonationsBackingFormValidatorTests.java
+++ src/test/java/org/jembi/bsis/validator/TestBatchDonationsBackingFormValidatorTests.java
@@ -1,37 +1,23 @@
 package org.jembi.bsis.validator;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.jembi.bsis.helpers.builders.DonationBuilder.aDonation;
-import static org.jembi.bsis.helpers.builders.TestBatchBuilder.aTestBatch;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.UUID;
-
 import org.jembi.bsis.backingform.TestBatchDonationsBackingForm;
 import org.jembi.bsis.backingform.validator.TestBatchDonationsBackingFormValidator;
-import org.jembi.bsis.model.donation.Donation;
-import org.jembi.bsis.model.testbatch.TestBatch;
-import org.jembi.bsis.model.testbatch.TestBatchStatus;
-import org.jembi.bsis.repository.DonationRepository;
-import org.jembi.bsis.repository.TestBatchRepository;
 import org.jembi.bsis.suites.UnitTestSuite;
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class TestBatchDonationsBackingFormValidatorTests extends UnitTestSuite {
 
   @InjectMocks
   private TestBatchDonationsBackingFormValidator validator;
-  @Mock
-  private DonationRepository donationRepository;
-  @Mock
-  private TestBatchRepository testBatchRepository;
 
   @Test
   public void testValidateNullForm_shouldReturnNoErrors() {
@@ -42,45 +28,10 @@ public class TestBatchDonationsBackingFormValidatorTests extends UnitTestSuite {
   }
 
   @Test
-  public void testValidateFormWithNullDonationIds_shouldReturnNoErrors() {
-    UUID testBatchId = UUID.randomUUID();
-    TestBatchDonationsBackingForm backingForm = new TestBatchDonationsBackingForm();
-    backingForm.setTestBatchId(testBatchId);
-    backingForm.setDonationIds(null);
-    TestBatch testBatch =
-        aTestBatch().withId(UUID.randomUUID()).withStatus(TestBatchStatus.OPEN).build();
-
-    when(testBatchRepository.findTestBatchById(testBatchId)).thenReturn(testBatch);
-
-    Errors errors = new BindException(backingForm, "testBatchDonationsBackingForm");
-    validator.validate(backingForm, errors);
-
-    assertThat(errors.getErrorCount(), is(0));
-  }
-
-  @Test
-  public void testValidateFormWithEmptyDonationIds_shouldReturnNoErrors() {
-    UUID testBatchId = UUID.randomUUID();
-    TestBatchDonationsBackingForm backingForm = new TestBatchDonationsBackingForm();
-    backingForm.setTestBatchId(testBatchId);
-    backingForm.setDonationIds(Collections.<UUID>emptyList());
-    TestBatch testBatch =
-        aTestBatch().withId(UUID.randomUUID()).withStatus(TestBatchStatus.OPEN).build();
-
-    when(testBatchRepository.findTestBatchById(testBatchId)).thenReturn(testBatch);
-
-    Errors errors = new BindException(backingForm, "testBatchDonationsBackingForm");
-    validator.validate(backingForm, errors);
-
-    assertThat(errors.getErrorCount(), is(0));
-  }
-
-  @Test
   public void testValidateFormWithNullTestBatchId_shouldReturnRequiredTestBatchIdError() {
-    UUID testBatchId = null;
     TestBatchDonationsBackingForm backingForm = new TestBatchDonationsBackingForm();
-    backingForm.setTestBatchId(testBatchId);
-    backingForm.setDonationIds(null);
+    backingForm.setTestBatchId(null);
+    backingForm.setDonationIds(Collections.singletonList(UUID.randomUUID()));
 
     Errors errors = new BindException(backingForm, "testBatchDonationsBackingForm");
     validator.validate(backingForm, errors);
@@ -91,93 +42,31 @@ public class TestBatchDonationsBackingFormValidatorTests extends UnitTestSuite {
   }
 
   @Test
-  public void testValidateFormWithInvalidTestBatchId_shouldReturnInvalidTestBatchIdError() {
-    UUID testBatchId = UUID.randomUUID();
+  public void testValidateFormWithNullDonationIds_shouldReturnNoErrors() {
     TestBatchDonationsBackingForm backingForm = new TestBatchDonationsBackingForm();
-    backingForm.setTestBatchId(testBatchId);
+    backingForm.setTestBatchId(UUID.randomUUID());
     backingForm.setDonationIds(null);
 
-    when(testBatchRepository.findTestBatchById(testBatchId)).thenReturn(null);
-
     Errors errors = new BindException(backingForm, "testBatchDonationsBackingForm");
     validator.validate(backingForm, errors);
 
     assertThat(errors.getErrorCount(), is(1));
     assertThat(errors.getFieldErrorCount(), is(1));
-    assertThat(errors.getFieldError("testBatchId").getCode(), is("errors.invalid"));
+    assertThat(errors.getFieldError("donationIds").getCode(), is("errors.required"));
   }
 
   @Test
-  public void testValidateFormWithReleasedTestBatch_shouldReturnTestBatchIsReleasedError() {
-    UUID testBatchId = UUID.randomUUID();
+  public void testValidateFormWithEmptyDonationIds_shouldReturnNoErrors() {
     TestBatchDonationsBackingForm backingForm = new TestBatchDonationsBackingForm();
-    backingForm.setTestBatchId(testBatchId);
-    backingForm.setDonationIds(null);
-    TestBatch testBatch =
-        aTestBatch().withId(testBatchId).withStatus(TestBatchStatus.RELEASED).build();
+    backingForm.setTestBatchId(UUID.randomUUID());
+    backingForm.setDonationIds(Collections.<UUID>emptyList());
 
-    when(testBatchRepository.findTestBatchById(testBatchId)).thenReturn(testBatch);
 
     Errors errors = new BindException(backingForm, "testBatchDonationsBackingForm");
     validator.validate(backingForm, errors);
 
     assertThat(errors.getErrorCount(), is(1));
     assertThat(errors.getFieldErrorCount(), is(1));
-    assertThat(errors.getFieldError("testBatchId").getCode(), is("errors.testBatchIsReleased"));
+    assertThat(errors.getFieldError("donationIds").getCode(), is("errors.required"));
   }
-
-  @Test
-  public void testValidateFormWithInvalidDonationId_shouldReturnInvalidDonationIdError() {
-    UUID testBatchId = UUID.randomUUID();
-    UUID donationId1 = UUID.randomUUID();
-    UUID donationId2 = UUID.randomUUID();
-    TestBatchDonationsBackingForm backingForm = new TestBatchDonationsBackingForm();
-    backingForm.setTestBatchId(testBatchId);
-    backingForm.setDonationIds(Arrays.asList(donationId1, donationId2));
-
-    TestBatch testBatch = aTestBatch().withId(testBatchId).withStatus(TestBatchStatus.OPEN).build();
-    Donation donation1 = aDonation().withId(donationId1).withTestBatch(testBatch).build();
-
-    when(donationRepository.findDonationById(donationId1)).thenReturn(donation1);
-    when(donationRepository.findDonationById(donationId2)).thenReturn(null);
-    when(testBatchRepository.findTestBatchById(testBatchId)).thenReturn(testBatch);
-
-    Errors errors = new BindException(backingForm, "testBatchDonationsBackingForm");
-    validator.validate(backingForm, errors);
-
-    assertThat(errors.getErrorCount(), is(1));
-    assertThat(errors.getFieldErrorCount(), is(1));
-    assertThat(errors.getFieldError("donationIds[1]").getCode(), is("errors.invalid"));
-  }
-
-  @Test
-  public void testValidateFormWithDonationFromAnotherBatch_shouldReturnFromAnotherBatchError() {
-    UUID testBatchId1 = UUID.randomUUID();
-    UUID testBatchId2 = UUID.randomUUID();
-    UUID donationId1 = UUID.randomUUID();
-    UUID donationId2 = UUID.randomUUID();
-    TestBatchDonationsBackingForm backingForm = new TestBatchDonationsBackingForm();
-    backingForm.setTestBatchId(testBatchId1);
-    backingForm.setDonationIds(Arrays.asList(donationId1, donationId2));
-
-    TestBatch testBatch1 =
-        aTestBatch().withId(testBatchId1).withStatus(TestBatchStatus.OPEN).build();
-    TestBatch testBatch2 =
-        aTestBatch().withId(testBatchId2).withStatus(TestBatchStatus.OPEN).build();
-    Donation donation1 = aDonation().withId(donationId1).withTestBatch(testBatch1).build();
-    Donation donation2 = aDonation().withId(donationId2).withTestBatch(testBatch2).build();
-
-    when(donationRepository.findDonationById(donationId1)).thenReturn(donation1);
-    when(donationRepository.findDonationById(donationId2)).thenReturn(donation2);
-    when(testBatchRepository.findTestBatchById(testBatchId1)).thenReturn(testBatch1);
-
-    Errors errors = new BindException(backingForm, "testBatchDonationsBackingForm");
-    validator.validate(backingForm, errors);
-
-    assertThat(errors.getErrorCount(), is(1));
-    assertThat(errors.getFieldErrorCount(), is(1));
-    assertThat(errors.getFieldError("donationIds[1]").getCode(),
-        is("errors.donationBelongsToAnotherTestBatch"));
-  }
-
 }
